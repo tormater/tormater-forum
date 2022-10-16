b36fa63aa77d9118309aa2eef285ac7251fffbc6
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2022-10-15 21:21:39 -0700

    Edit embed

--- pages/header.php
+++ pages/header.php
@@ -9,11 +9,11 @@ if (!defined("INDEXED")) exit;
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="nl" lang="nl">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta name="description" content="A short description." />
-	<meta name="keywords" content="put, keywords, here" />
+	<meta name="description" content="Powered by tormater-forum" />
+	<meta name="keywords" content="tormater-forum, tormater, forum" />
 	<title><?php echo $config["forumName"]; ?></title>
 
 <?php
