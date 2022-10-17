ea9ca15e1fb27f53ae618161ea25729965b7fe35
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2022-10-17 10:41:06 -0700

    Add english language file

---
+++ lang/EN_US.php
@@ -0,0 +1,13 @@
+<?php
+// EN_US.php
+// English, United States language file
+
+// Only load the page if it's being loaded through the index.php file.
+if (!defined("INDEXED")) exit;
+
+$lang = array(
+"error.PageNotFound" => "Requested page not found.",
+
+);
+
+?>
