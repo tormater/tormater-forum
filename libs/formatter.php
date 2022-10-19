2ce7d2be6629037fc8cbf43b6a181b7639e8c47b
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2022-10-19 10:01:42 -0700

    New formatter, though still not ideal
    Using preg_replace is still better than using str_replace but this change is temporary until I find a better way to format BBCode. The issue is, I don't want to use some massive library for it, and so I'll have to recode it later.

--- libs/formatter.php
+++ libs/formatter.php
@@ -8,143 +8,38 @@ if (!defined("INDEXED")) exit;
 function formatPost($post)
 {
     $returnPost = htmlspecialchars($post);
-    $returnPost = formatNewline($returnPost);
-    $returnPost = formatBold($returnPost);
-    $returnPost = formatItalic($returnPost);
-    $returnPost = formatUnderline($returnPost);
-    $returnPost = formatStrikethrough($returnPost);
-    $returnPost = formatPreformatted($returnPost);
-    $returnPost = formatCode($returnPost);
-    $returnPost = formatTable($returnPost);
-    $returnPost = formatTablerow($returnPost);
-    $returnPost = formatTabledata($returnPost);
-    $returnPost = formatTableheader($returnPost);
-    $returnPost = formatLink($returnPost);
-    $returnPost = formatImage($returnPost);
-    $returnPost = formatSpoiler($returnPost);
+    $returnPost = str_replace("\n","</br>",$returnPost);
+    $find = array(
+        '/\[b\](.*?)\[\/b\]/is',
+        '/\[i\](.*?)\[\/i\]/is',
+        '/\[u\](.*?)\[\/u\]/is',
+        '/\[s\](.*?)\[\/s\]/is',
+        '/\[url\](.*?)\[\/url\]/is',
+        '/\[url=(.*?)\](.*?)\[\/url\]/is',
+        '/\[img\](.*?)\[\/img\]/is',
+        '/\[spoiler\](.*?)\[\/spoiler\]/is',
+        '/\[spoiler=(.*?)\](.*?)\[\/spoiler\]/is',
+        '/\[color=(.*?)\](.*?)\[\/color\]/is',
+        '/\[code\](.*?)\[\/code\]/is',
+        '/\[pre\](.*?)\[\/pre\]/is'
+    );
+
+    $replace = array(
+        '<b>$1</b>',
+        '<i>$1</i>',
+        '<u>$1</u>',
+        '<s>$1</s>',
+        '<a href="$1" target="_blank">$1</a>',
+        '<a href="$2" target="_blank">$1</a>',
+        '<img class="postimg" src="$1">',
+        '<details class="postspoiler"><summary>Spoiler</summary>$1</details>',
+        '<details class="postspoiler"><summary>Spoiler: $1</summary>$2</details>',
+        '<font color="$1">$2</font>',
+        '<code class="postcode">$1</code>',
+        '<pre class="postpre">$1</pre>'
+    );
+
+    $returnPost = preg_replace($find, $replace, $returnPost);
     return $returnPost;
 }
-
-function formatNewline($returnPost)
-{
-    return str_replace("\n","</br>",$returnPost);
-}
-
-function formatBold($returnPost)
-{
-    if ((strpos($returnPost, "[b]") !== false) and (strpos($returnPost, "[/b]") !== false)) {
-        $returnPost = str_replace("[b]","<b>",$returnPost);
-        $returnPost = str_replace("[/b]","</b>",$returnPost);
-    }
-    return $returnPost;
-}
-
-function formatItalic($returnPost)
-{
-    if ((strpos($returnPost, "[i]") !== false) and (strpos($returnPost, "[/i]") !== false)) {
-        $returnPost = str_replace("[i]","<i>",$returnPost);
-        $returnPost = str_replace("[/i]","</i>",$returnPost);
-    }
-    return $returnPost;
-}
-
-function formatUnderline($returnPost)
-{
-    if ((strpos($returnPost, "[u]") !== false) and (strpos($returnPost, "[/u]") !== false)) {
-        $returnPost = str_replace("[u]","<u>",$returnPost);
-        $returnPost = str_replace("[/u]","</u>",$returnPost);
-    }
-    return $returnPost;
-}
-
-function formatStrikethrough($returnPost)
-{
-    if ((strpos($returnPost, "[s]") !== false) and (strpos($returnPost, "[/s]") !== false)) {
-        $returnPost = str_replace("[s]","<s>",$returnPost);
-        $returnPost = str_replace("[/s]","</s>",$returnPost);
-    }
-    return $returnPost;
-}
-
-function formatPreformatted($returnPost)
-{
-    if ((strpos($returnPost, "[pre]") !== false) and (strpos($returnPost, "[/pre]") !== false)) {
-        $returnPost = str_replace("[pre]","<pre class='postpre'>",$returnPost);
-        $returnPost = str_replace("[/pre]","</pre>",$returnPost);
-    }
-    return $returnPost;
-}
-
-function formatCode($returnPost)
-{
-    if ((strpos($returnPost, "[code]") !== false) and (strpos($returnPost, "[/code]") !== false)) {
-        $returnPost = str_replace("[code]","<code class='postcode'>",$returnPost);
-        $returnPost = str_replace("[/code]","</code>",$returnPost);
-    }
-    return $returnPost;
-}
-
-function formatTable($returnPost)
-{
-    if ((strpos($returnPost, "[table]") !== false) and (strpos($returnPost, "[/table]") !== false)) {
-        $returnPost = str_replace("[table]","<table>",$returnPost);
-        $returnPost = str_replace("[/table]","</table>",$returnPost);
-    }
-    return $returnPost;
-}
-
-function formatTablerow($returnPost)
-{
-    if ((strpos($returnPost, "[tr]") !== false) and (strpos($returnPost, "[/tr]") !== false)) {
-        $returnPost = str_replace("[tr]","<tr>",$returnPost);
-        $returnPost = str_replace("[/tr]","</tr>",$returnPost);
-    }
-    return $returnPost;
-}
-
-function formatTabledata($returnPost)
-{
-    if ((strpos($returnPost, "[td]") !== false) and (strpos($returnPost, "[/td]") !== false)) {
-        $returnPost = str_replace("[td]","<td>",$returnPost);
-        $returnPost = str_replace("[/td]","</td>",$returnPost);
-    }
-    return $returnPost;
-}
-
-function formatTableheader($returnPost)
-{
-    if ((strpos($returnPost, "[th]") !== false) and (strpos($returnPost, "[/th]") !== false)) {
-        $returnPost = str_replace("[th]","<th>",$returnPost);
-        $returnPost = str_replace("[/th]","</th>",$returnPost);
-    }
-    return $returnPost;
-}
-
-function formatLink($returnPost)
-{
-    if ((strpos($returnPost, "[url]http") !== false) and (strpos($returnPost, "[/url]") !== false)) {
-        $returnPost = str_replace("[url]http","<a class='postlink' href='http",$returnPost);
-        $returnPost = str_replace("[/url]","'>link text</a>",$returnPost);
-    }
-    return $returnPost;
-}
-
-function formatImage($returnPost)
-{
-    if ((strpos($returnPost, "[img]http") !== false) and (strpos($returnPost, "[/img]") !== false)) {
-        $returnPost = str_replace("[img]http","<img class='postimg' src='http",$returnPost);
-        $returnPost = str_replace("[/img]","'>",$returnPost);
-    }
-    return $returnPost;
-}
-
-function formatSpoiler($returnPost)
-{
-    if ((strpos($returnPost, "[spoiler]") !== false) and (strpos($returnPost, "[/spoiler]") !== false)) {
-        $returnPost = str_replace("[spoiler]","<details class='postspoiler'><summary>Spoiler</summary>",$returnPost);
-        $returnPost = str_replace("[/spoiler]","</details>",$returnPost);
-    }
-    return $returnPost;
-}
-
 ?>
