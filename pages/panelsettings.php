95b8ff496fab9e1923a9136272f9d907bf9022a1
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2022-10-23 18:08:41 -0700

    Add forum language setting, modify footer setting

--- pages/panelsettings.php
+++ pages/panelsettings.php
@@ -14,12 +14,6 @@ if($_SERVER['REQUEST_METHOD'] != 'POST')
     <div class="formcontainer"><form method="post" action="">
     <label>' . $lang["panel.NewForumName"]  . '</label><input type="text" name="newforumname" value="' . $config["forumName"] . '"><br/>
     <label></label><input type="submit" class="buttoninput" value="' . $lang["panel.ChangeForumName"]  . '"></form></div>';
-    
-    // Display the change footer form.
-	echo '<h3>' . $lang["panel.ChangeFooter"]  . '</h3>
-    <div class="formcontainer"><form method="post" action="">
-    <label>' . $lang["panel.NewFooter"]  . '</label><input type="text" name="newfooter" value="' . htmlspecialchars($config["footer"]) . '"><br/>
-    <label></label><input type="submit" class="buttoninput" value="' . $lang["panel.ChangeFooter"]  . '"></form></div>';
 
     // Display the change theme form.
 	echo '<h3>' . $lang["panel.ChangeTheme"]  . '</h3>
@@ -39,8 +33,33 @@ if($_SERVER['REQUEST_METHOD'] != 'POST')
         }
         echo '<option ' . $selected . 'value="' . $file . '">' . $file . '</option>';
     }
-    //<input type="text" name="newtheme" value="' . htmlspecialchars($config["forumTheme"]) . '"><br/>
     echo '</select><br/><label></label><input type="submit" class="buttoninput" value="' . $lang["panel.ChangeTheme"]  . '"></form></div>';
+
+    // Display the change language form.
+	echo '<h3>' . $lang["panel.ChangeLang"]  . '</h3>
+    <div class="formcontainer"><form method="post" action="">
+    <label>' . $lang["panel.NewLang"]  . '</label>';
+    echo '<select name="newlang">';
+    $files = scandir(dirname(__DIR__,1) . '/lang');
+    foreach($files as $file) {
+        if($file == '.' || $file == '..') continue;
+        if(substr($file, 0, -4) == $config["forumLang"]) 
+        {
+            $selected = "selected ";
+        }
+        else
+        {
+            $selected = "";
+        }
+        echo '<option ' . $selected . 'value="' . substr($file, 0, -4) . '">' . substr($file, 0, -4) . '</option>';
+    }
+    echo '</select><br/><label></label><input type="submit" class="buttoninput" value="' . $lang["panel.ChangeLang"]  . '"></form></div>';
+
+    // Display the change footer form.
+	echo '<h3>' . $lang["panel.ChangeFooter"]  . '</h3>
+    <div class="formcontainer"><form method="post" action="">
+    <textarea name="newfooter">' . htmlspecialchars($config["footer"]) . '</textarea><br/>
+    <input type="submit" class="buttoninput" value="' . $lang["panel.ChangeFooter"]  . '"></form></div>';
 }
 
 else
@@ -70,6 +89,16 @@ else
 		$newtheme = $_POST["newtheme"];
 		
         $config["forumTheme"] = $newtheme;
+        saveConfig("./config/config.php", $config);
+        echo '<meta http-equiv="refresh" content="1;url=/panel/" />';
+        message("Your changes were saved.");
+	}
+
+	if (isset($_POST["newlang"]))
+	{
+		$newlang = $_POST["newlang"];
+		
+        $config["forumLang"] = $newlang;
         saveConfig("./config/config.php", $config);
         echo '<meta http-equiv="refresh" content="1;url=/panel/" />';
         message("Your changes were saved.");
