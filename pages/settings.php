8b8edc7f90f095d710e0d1688efc168dc4344a3c
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2022-10-30 10:59:23 -0700

    Update color display

--- pages/settings.php
+++ pages/settings.php
@@ -14,7 +14,7 @@ if (empty($_SESSION["signed_in"]))
 	exit;
 }
 
-if (isset($q3) && is_numeric($q3) && ($q2 == "color"))
+if (isset($q3) && is_numeric($q3) && ($q2 == "color") && ($q3 !== 0) && ($q3 <= 16))
 {
 	$result = $db->query("UPDATE users SET color='" . $db->real_escape_string($q3) . "' WHERE userid='" . $_SESSION["userid"] . "'");
 	
@@ -31,13 +31,23 @@ echo '<h2>Settings</h2>';
 if($_SERVER['REQUEST_METHOD'] != 'POST')
 {
 	// Display the post color setting.
-	echo "<fieldset><legend>Post color</legend><a href='/settings/color/1/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='lightblue' /></svg></a><a href='/settings/color/2/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='lightgreen' /></svg></a><a href='/settings/color/3/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='yellow' /></svg></a><a href='/settings/color/4/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='indianred' /></svg></a><a href='/settings/color/5/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='hotpink' /></svg></a><a href='/settings/color/6/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='mediumpurple' /></svg></a><a href='/settings/color/7/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='coral' /></svg></a><a href='/settings/color/8/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='cornflowerblue' /></svg></a><a href='/settings/color/9/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='lavender' /></svg></a><a href='/settings/color/10/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='silver' /></svg></a><a href='/settings/color/11/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='wheat' /></svg></a><a href='/settings/color/12/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='sandybrown' /></svg></a><a href='/settings/color/13/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='mediumseagreen' /></svg></a><a href='/settings/color/14/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='gold' /></svg></a><a href='/settings/color/15/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='plum' /></svg></a><a href='/settings/color/16/'><svg width='50' height='50'><circle cx='25' cy='25' r='20' fill='aliceblue' /></svg></a></fieldset><br/>";
+	echo "<h3>Post color</h3><div class='formcontainer'>";
+
+    $i = 1;
+
+    while ($i <= 16) {
+    echo "<a class='settingscolor' postcolor='" . $i . "' href='/settings/color/" . $i . "/'></a>";
+    $i++;
+    }
+
+    echo "</div>";
+
 	// Display the change username form.
-	echo '<fieldset><legend>Change username</legend>
+	echo '<h3>Change username</h3><div class="formcontainer">
     <form method="post" action="">
-    <label>New username:</label><input type="text" name="newusername"><br/><br/>
-    <label>Current password:</label><input type="password" name="confirmpass"><br/><br/>
-    <label></label><input type="submit" class="buttoninput" value="Change username"></form></fieldset>';
+    <label>New username</label><input type="text" name="newusername"><br/>
+    <label>Current password</label><input type="password" name="confirmpass"><br/>
+    <label></label><input type="submit" class="buttonbig" value="Change username"></form></div>';
 }
 
 else
