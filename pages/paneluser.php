6e3c2cb8c58acf7d44cc380e2810d8ba85c8f451
GigaHacer <94160071+GigaHacer@users.noreply.github.com>
2022-11-02 10:02:33 -0700

    Add users tab to panel

---
+++ pages/paneluser.php
@@ -0,0 +1,36 @@
+<?php
+// paneluser.php
+// Displays a list of all the users on the forum for admins to administrate.
+
+// Only load the page if it's being loaded through the index.php file.
+if (!defined("INDEXED")) exit;
+
+// Start off by making a query for our list.
+$result = $db->query("SELECT * FROM users ORDER BY userid ASC");
+
+if (!$result)
+{
+	echo "Failed to fetch users.";
+}
+
+else
+{
+	if ($result->num_rows == 0)
+	{
+		echo "Sadly, there are currently no users on the forum.";
+	}
+	
+	else
+	{
+		echo '<h2>Users</h2>';
+
+        
+		
+		while($row = $result->fetch_assoc())
+		{
+			echo '<div class="userlist" postcolor="' . $row["color"] . '"><b><a href="/user/' . $row["userid"] . '/" id="' . $row["role"] . '">' . htmlspecialchars($row["username"]) . '</a></b>&nbsp; ' . $row["role"] . '&nbsp; <small>' . parseAction($row["lastaction"], $lang) . ' (<a class="date" title="' . date('m-d-Y h:i:s A', $row["lastactive"]) . '">' . relativeTime($row["lastactive"]) . '</a>)</small></div></br>';
+		}
+	}
+}
+
+?>
