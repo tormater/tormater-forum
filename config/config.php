a7cd182ba3c6b0531f6cbcc0213a4516fdcbd8bd
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2022-11-03 08:19:44 -0700

    Use single quotes by default, rather than double quotes

--- config/config.php
+++ config/config.php
@@ -3,28 +3,28 @@
 // Contains all the config options essential for the forum to function properly.
 
 // Only load the page if it's being loaded through the index.php file.
-if (!defined("INDEXED")) exit;
+if (!defined('INDEXED')) exit;
 
 $config = array(
-    "installed" => "no", // Whether or not the forum is installed. (yes/no)
-    "MySQLServer" => "", // URL for your MySQL server. Set to localhost if it's on the same machine.
-    "MySQLUser" => "", // Username for native MySQL user.
-    "MySQLPass" => "", // Password for native MySQL user.
-    "MySQLDatabase" => "", // Name of the MySQL database you wish to use.
-    "baseURL" => "", // The base URL for the website that the forum is on.
-    "cookieName" => "forum", // The name of cookies set by the forum.
-    "forumName" => "Tormater Forum", // The forum's name.
-    "footer" => "Powered by [url=tormater-forum]https://github.com/tormater/tormater-forum[/url]", // The text that appears on the footer.
-    "postsPerPage" => 10, // Number of posts to display on a page in a thread.
-    "threadsPerPage" => 20, // Number of threads to display on a page in a category.
-    "maxCharsPerPost" => 50000, // Maximum number of characters allowable in a single post.
-    "postDelay" => 5, // Number of seconds between posts during which a user can't make another post.
-    "maxCats" => 10, // Maximum number of categories that can be created.
-    "maxCharsPerTitle" => 35, // Maximum number of characters allowable in a title. Includes category names along with thread titles.
-    "forumTheme" => "Skyline", // The directory that the forum should look in for skin files
-    "forumLang" => "EN_US", // The language file that the forum should use to display strings
-    "userlistEnabled" => true, // Whether or not the userlist is enabled.
-    "userlistMembersOnly" => false // Whether or not the userlist is only viewable by members.
+    'installed' => 'no', // Whether or not the forum is installed. (yes/no)
+    'MySQLServer' => '', // URL for your MySQL server. Set to localhost if it's on the same machine.
+    'MySQLUser' => '', // Username for native MySQL user.
+    'MySQLPass' => '', // Password for native MySQL user.
+    'MySQLDatabase' => '', // Name of the MySQL database you wish to use.
+    'baseURL' => '', // The base URL for the website that the forum is on.
+    'cookieName' => 'forum', // The name of cookies set by the forum.
+    'forumName' => 'Tormater Forum', // The forum's name.
+    'footer' => 'Powered by [url=tormater-forum]https://github.com/tormater/tormater-forum[/url]', // The text that appears on the footer.
+    'postsPerPage' => 10, // Number of posts to display on a page in a thread.
+    'threadsPerPage' => 20, // Number of threads to display on a page in a category.
+    'maxCharsPerPost' => 50000, // Maximum number of characters allowable in a single post.
+    'postDelay' => 5, // Number of seconds between posts during which a user can't make another post.
+    'maxCats' => 10, // Maximum number of categories that can be created.
+    'maxCharsPerTitle' => 35, // Maximum number of characters allowable in a title. Includes category names along with thread titles.
+    'forumTheme' => 'Skyline', // The directory that the forum should look in for skin files
+    'forumLang' => 'EN_US', // The language file that the forum should use to display strings
+    'userlistEnabled' => true, // Whether or not the userlist is enabled.
+    'userlistMembersOnly' => false // Whether or not the userlist is only viewable by members.
 );
 
 ?>
