068c6efc915e08eef3f8b0ce1bcaf563cd72fde6
菜包 <36061682+vegbao@users.noreply.github.com>
2022-11-11 15:45:47 +0800

    Update Forum Settings page

--- pages/panelsettings.php
+++ pages/panelsettings.php
@@ -10,38 +10,12 @@ echo '<h2>' . $lang["panel.ForumSettings"]  . '</h2>';
 if($_SERVER['REQUEST_METHOD'] != 'POST')
 {
 	// Display the change forum name form.
-	echo '<h3>' . $lang["panel.ChangeForumName"]  . '</h3>
-    <div class="formcontainer"><form method="post" action="">
-    <div class="forminput"><label>' . $lang["panel.NewForumName"]  . '</label><input type="text" name="newforumname" value="' . $config["forumName"] . '"></div>
-    <div class="forminput"><label></label><input type="submit" class="buttonbig" value="' . $lang["panel.ChangeForumName"]  . '"></div>
-    </form></div>';
-
-    // Display the change theme form.
-	echo '<h3>' . $lang["panel.ChangeTheme"]  . '</h3>
-    <div class="formcontainer"><form method="post" action="">
-    <div class="forminput"><label>' . $lang["panel.NewTheme"]  . '</label>';
-    echo '<select name="newtheme">';
-    $files = scandir(dirname(__DIR__,1) . '/themes');
-    foreach($files as $file) {
-        if($file == '.' || $file == '..') continue;
-        if($file == $config["forumTheme"]) 
-        {
-            $selected = "selected ";
-        }
-        else
-        {
-            $selected = "";
-        }
-        echo '<option ' . $selected . 'value="' . $file . '">' . $file . '</option>';
-    }
-    echo '</select></div>
-    <div class="forminput"><label></label><input type="submit" class="buttonbig" value="' . $lang["panel.ChangeTheme"]  . '"></div>
-    </form></div>';
+	echo '<h3>' . $lang["panel.BasicSettings"] . '</h3>
+   <form method="post" action=""><div class="formcontainer">
+    <div class="forminput"><label>' . $lang["panel.NewForumName"]  . '</label><input type="text" name="newforumname" value="' . $config["forumName"] . '"></div>';
 
     // Display the change language form.
-	echo '<h3>' . $lang["panel.ChangeLang"]  . '</h3>
-    <div class="formcontainer"><form method="post" action="">
-    <div class="forminput"><label>' . $lang["panel.NewLang"]  . '</label>';
+	echo '<div class="forminput"><label>' . $lang["panel.NewLang"]  . '</label>';
     echo '<select name="newlang">';
     $files = scandir(dirname(__DIR__,1) . '/lang');
     foreach($files as $file) {
@@ -56,57 +30,97 @@ if($_SERVER['REQUEST_METHOD'] != 'POST')
         }
         echo '<option ' . $selected . 'value="' . substr($file, 0, -4) . '">' . substr($file, 0, -4) . '</option>';
     }
-    echo '</select></div>
-    <div class="forminput"><label></label><input type="submit" class="buttonbig" value="' . $lang["panel.ChangeLang"]  . '"></div>
-    </form></div>';
+    echo '</select></div>';
+
+    // Display the change theme form.
+	echo '<div class="forminput"><label>' . $lang["panel.NewTheme"]  . '</label>';
+    echo '<select name="newtheme">';
+    $files = scandir(dirname(__DIR__,1) . '/themes');
+    foreach($files as $file) {
+        if($file == '.' || $file == '..') continue;
+        if($file == $config["forumTheme"]) 
+        {
+            $selected = "selected ";
+        }
+        else
+        {
+            $selected = "";
+        }
+        echo '<option ' . $selected . 'value="' . $file . '">' . $file . '</option>';
+    }
+    echo '</select></div></div>';
+
+    // Display the advanced settings.
+	echo '<h3>' . $lang["panel.AdvancedSettings"] . '</h3>';
+    echo '<div class="formcontainer">
+    <div class="forminput"><label>' . $lang["panel.ThreadsPerPage"] . '</label>
+    <input type="text" name="newthreadsPerPage" value="' . $config["threadsPerPage"] . '"></div>
+    <div class="forminput"><label>' . $lang["panel.PostPerPage"] . '</label>
+    <input type="text" name="newpostsPerPage" value="' . $config["postsPerPage"] . '"></div>
+    <div class="forminput"><label>' . $lang["panel.Userlist"] . '</label>
+    <input type="checkbox" name="newuserlistEnabled" id="userlist" value="'. $config["userlistEnabled"] .'" ';
+    if ($config["userlistEnabled"] == 1)
+    {
+        echo 'checked';
+    }
+    echo '> <label for="userlist">' . $lang["panel.EnabledBtn"] . '</label></div>
+    <div class="forminput"><label>' . $lang["panel.UserlistMemberOnly"] . '</label>
+    <input type="checkbox" name="newuserlistMembersOnly" id="userlistMember" value="'. $config["userlistMembersOnly"] .'" ';
+    if ($config["userlistMembersOnly"] == 1)
+    {
+        echo 'checked';
+    }
+    echo '> <label for="userlistMember">' . $lang["panel.EnabledBtn"] . '</label></div></div>';
 
     // Display the change footer form.
 	echo '<h3>' . $lang["panel.ChangeFooter"]  . '</h3>
-    <div class="formcontainer"><form method="post" action="">
+    <div class="formcontainer">
     <div class="forminput"><textarea name="newfooter">' . htmlspecialchars($config["footer"]) . '</textarea></div>
-    <div class="forminput"><input type="submit" class="buttonbig" value="' . $lang["panel.ChangeFooter"]  . '"></form></div></div>';
+    <div class="forminput"><input type="submit" class="buttonbig" value="' . $lang["panel.ChangeSettingsBtn"] . '"></div></div></form>';
 }
 
 else
 {
-	if (isset($_POST["newforumname"]))
+	if (!isset($_POST))
 	{
-		$newforumname = $_POST["newforumname"];
-		
-        $config["forumName"] = htmlspecialchars($newforumname);
-        saveConfig("./config/config.php", $config);
-        echo '<meta http-equiv="refresh" content="1;url=/panel/" />';
-        message($lang["panel.ChangesSaved"]);
+        message($lang["panel.ChangeError"]);
 	}
-
-	if (isset($_POST["newfooter"]))
-	{
+    else
+    {
+        if(isset($_POST["newuserlistEnabled"]))
+        {
+            $newuserlistEnabled = 1;
+        }
+        else
+        {
+            $newuserlistEnabled = 0;
+        }
+        if(isset($_POST["newuserlistMembersOnly"]))
+        {
+            $newuserlistMembersOnly = 1;
+        }
+        else
+        {
+            $newuserlistMembersOnly = 0;
+        }
+		$newforumname = $_POST["newforumname"];
+        $newthreadsPerPage = $_POST["newthreadsPerPage"];
+        $newpostsPerPage = $_POST["newpostsPerPage"];
+		$newtheme = $_POST["newtheme"];
+		$newlang = $_POST["newlang"];
 		$newfooter = $_POST["newfooter"];
-		
+        
+        $config["forumName"] = htmlspecialchars($newforumname);
+        $config["threadsPerPage"] = $newthreadsPerPage;
+        $config["postsPerPage"] = $newpostsPerPage;
+        $config['userlistEnabled'] = $newuserlistEnabled;
+        $config['userlistMembersOnly'] = $newuserlistMembersOnly;
+        $config["forumTheme"] = $newtheme;
+        $config["forumLang"] = $newlang;
         $config["footer"] = $newfooter;
         saveConfig("./config/config.php", $config);
         echo '<meta http-equiv="refresh" content="1;url=/panel/" />';
         message($lang["panel.ChangesSaved"]);
-	}
-
-	if (isset($_POST["newtheme"]))
-	{
-		$newtheme = $_POST["newtheme"];
-		
-        $config["forumTheme"] = $newtheme;
-        saveConfig("./config/config.php", $config);
-        echo '<meta http-equiv="refresh" content="1;url=/panel/" />';
-        message($lang["panel.ChangesSaved"]);
-	}
-
-	if (isset($_POST["newlang"]))
-	{
-		$newlang = $_POST["newlang"];
-		
-        $config["forumLang"] = $newlang;
-        saveConfig("./config/config.php", $config);
-        echo '<meta http-equiv="refresh" content="1;url=/panel/" />';
-        message($lang["panel.ChangesSaved"]);
-	}
+    }
 }
 ?>
