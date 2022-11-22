7964f78a3d57982a5898efab742ae63601d99e83
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2022-11-21 19:57:17 -0800

    Update paneluser.php

--- pages/paneluser.php
+++ pages/paneluser.php
@@ -23,13 +23,35 @@ else
 	else
 	{
 		echo '<h2>'.$lang["panel.Users"].'</h2>';
-
+        echo "<div class='userlist-grid'>";
         
 		
 		while($row = $result->fetch_assoc())
 		{
-			echo '<div class="userlist" postcolor="' . $row["color"] . '"><b><a href="' . genURL('user/' . $row["userid"]) . '/" id="' . $row["role"] . '">' . htmlspecialchars($row["username"]) . '</a></b>&nbsp; ' . $row["role"] . '&nbsp; <small>' . parseAction($row["lastaction"], $lang) . ' (<a class="date" title="' . date('m-d-Y h:i:s A', $row["lastactive"]) . '">' . relativeTime($row["lastactive"]) . '</a>)</small></div>';
+			if ($row["role"] == "Administrator")
+			{
+				$role = $lang["role.Admin"];
+			}
+			elseif ($row["role"] == "Moderator")
+			{
+				$role = $lang["role.Mod"];
+			}
+			elseif ($row["role"] == "Member")
+			{
+				$role = $lang["role.Member"];
+			}
+			elseif ($row["role"] == "Suspended")
+			{
+				$role = $lang["role.Suspend"];
+			}
+			else
+			{
+				$role = $lang["role.Member"];
+			}
+
+			echo '<div class="userlist"><div class="userlist-top" postcolor="' . $row["color"] . '"><b><a href="' . genURL('user/' . $row["userid"]) . '/" id="' . $row["role"] . '">' . htmlspecialchars($row["username"]) . '</a></b>&nbsp; ' . $role . '&nbsp; <small></div><div class="userlist-bottom">' . parseAction($row["lastaction"], $lang) . ' (<a class="date" title="' . date('m-d-Y h:i:s A', $row["lastactive"]) . '">' . relativeTime($row["lastactive"]) . '</a>)</small></div></div>';
 		}
+        echo "</div>";
 	}
 }
 
