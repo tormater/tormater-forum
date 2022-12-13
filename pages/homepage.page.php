c3a91cd61cd524290bfd367a8d22069da7aa42b8
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2022-12-12 17:53:26 -0800

    Update Homepage

--- pages/homepage.page.php
+++ pages/homepage.page.php
@@ -7,20 +7,67 @@ if (!defined("INDEXED")) exit;
 
 include 'header.php';
 
-echo '<h2>' . $lang["homepage.Cats"] . '</h2>';
+echo '<h2>' . $lang["homepage.Title"] . '</h2>';
 
 $result = $db->query("SELECT * FROM categories");
 
+$threads = $db->query("SELECT * FROM threads ORDER BY lastposttime DESC LIMIT 5");
+
+echo '<table><tr><th>' . $lang["homepage.Cats"] . '</th></tr>';
 while($row = $result->fetch_assoc()) {
 	$numthreads = $db->query("SELECT * FROM threads WHERE category='" . $row["categoryid"] . "'");
 	$number = $numthreads->num_rows;
-	echo '<div class="category"><tr>';
-		echo '<td class="leftpart">';
-			echo '<h3><a href="' . genURL("category/" . $row["categoryid"]) . '">' . htmlspecialchars($row["categoryname"]) . '</a></h3>';
-		echo '<div>' . htmlspecialchars($row["categorydescription"]) . '</div>';
-		echo '<div>' . $lang["homepage.CatThreads"] . $number . '</div>';
-	echo '</tr></div>';
+	echo '<tr><td><h3><a href="' . genURL("category/" . $row["categoryid"]) . '">' . htmlspecialchars($row["categoryname"]) . '</a></h3>';
+	echo '<div>' . htmlspecialchars($row["categorydescription"]) . '</div>';
+	echo '<div>' . $lang["homepage.CatThreads"] . $number . '</div></td></tr>';
 }
+echo '</table>';
+echo '<table><tr><th>' . $lang["homepage.Threads"] . '</th><th>' . $lang["category.Posts"] . '</th><th>' . $lang["category.CreatedBy"] . '</th><th>' . $lang["category.LastPost"] . '</th></tr>';
+					
+while($row = $threads->fetch_assoc())
+{				
+	echo '<tr><td class="leftpart">';
+
+	if ($row["locked"] == 1)
+	{
+		echo '<span class="locked">' . $lang["label.Locked"] . '</span>';
+	}
+	
+	if ($row["sticky"] == 1)
+	{
+		echo '<span class="sticky">' . $lang["label.Sticky"] . '</span>';
+	}
+
+    echo '<b><a href="' . genURL('thread/' . $row['threadid']) . '">' . htmlspecialchars($row['title']) . "</a></b>";	
+
+
+	echo '</td><td><center>' . $row['posts'] . '</center></td><td>';
+					
+	$uinfo = $db->query("SELECT * FROM users WHERE userid='" . $row["startuser"] . "'");
+					
+	while ($u = $uinfo->fetch_assoc())
+	{
+		echo '<a href="' . genURL('user/' . $row['startuser']) . '" id="' . $u["role"] . '">' . $u['username'] . '</a>';
+	}
+					
+	echo "<div class='tddate' title='" . date('m-d-Y h:i:s A', $row['starttime']) . "'>" . relativeTime($row["starttime"]) . "</div>";
+					
+	echo '</td><td>';
+					
+	$uinfo = $db->query("SELECT * FROM users WHERE userid='" . $row["lastpostuser"] . "'");
+					
+	while ($u = $uinfo->fetch_assoc())
+	{
+		echo '<a href="' . genURL('user/' . $row['lastpostuser']) . '" id="' . $u["role"] . '">' . $u['username'] . '</a>';
+	}
+					
+	echo '<div class="tddate" title="' . date('m-d-Y h:i:s A', $row['lastposttime']) . '">' . relativeTime($row["lastposttime"]) . '</div></td></tr>';
+}
+
+echo "</table>";
+
+
+// Include "latest threads"
 
 include 'footer.php';
 
