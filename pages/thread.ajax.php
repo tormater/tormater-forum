0be3f51e66e2217aa05b47cd7116869bc56d0148
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2023-01-16 09:12:07 -0800

    You cannot quote a hidden post.

--- pages/thread.ajax.php
+++ pages/thread.ajax.php
@@ -6,15 +6,18 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
 	if ($_POST["postid"] && is_numeric($_POST["postid"]))
 	{
-        $post = $db->query("SELECT content FROM posts WHERE postid='" . $db->real_escape_string($_POST["postid"]) . "'");
+        $post = $db->query("SELECT content, deletedby FROM posts WHERE postid='" . $db->real_escape_string($_POST["postid"]) . "'");
         while($row = $post->fetch_assoc())
 	    {
-            echo "[quote]";
-            $quotecontent = preg_replace("/\[quote\](.+?)\[\/quote\]/is", "", htmlspecialchars($row["content"]));
-            $quotecontent = preg_replace("/\[blockquote\](.+?)\[\/blockquote\]/is", "", $quotecontent);
-            echo $quotecontent;
-            echo "[/quote]";
-            echo "\n";
+            if (!isset($row["deletedby"]))
+            {
+                echo "[quote]";
+                $quotecontent = preg_replace("/\[quote\](.+?)\[\/quote\]/is", "", htmlspecialchars($row["content"]));
+                $quotecontent = preg_replace("/\[blockquote\](.+?)\[\/blockquote\]/is", "", $quotecontent);
+                echo $quotecontent;
+                echo "[/quote]";
+                echo "\n";
+            }
         }
     }
 }
