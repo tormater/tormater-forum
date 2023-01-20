258b6b2f03933d4339391143f845a38e265c41f1
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2023-01-19 18:37:40 -0800

    Add javascript for post previews

--- assets/thread.js
+++ assets/thread.js
@@ -17,3 +17,37 @@ function quotePost(id) {
     xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhttp.send('postid=' + id); 
 }
+
+function previewPost(num) {
+    let ajaxPath = new URL(baseURL + '/pages/thread.ajax.php');
+    var Field = document.getElementById('textbox' + num);
+    var PreviewBox = document.getElementById("previewbox" + num)
+    var PreviewButton = document.getElementById("showpreview" + num)
+    var HidePreviewButton = document.getElementById("hidepreview" + num)
+    var val = Field.value;
+
+    if (PreviewBox.innerHTML == "") {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                Field.setSelectionRange(Field.selectionStart,Field.selectionEnd);
+                PreviewButton.style = "display: none";
+                HidePreviewButton.style = "";
+                PreviewBox.innerHTML = this.responseText;
+                PreviewBox.style = "";
+                PreviewButton.blur()
+           }
+        };
+    
+        xhttp.open('POST', ajaxPath, true);
+        xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhttp.send('postRaw=' + Field.value); 
+    }
+    else {
+        PreviewButton.style = "";
+        HidePreviewButton.style = "display: none";
+        PreviewBox.style = "display: none";
+        PreviewBox.innerHTML = ""
+        HidePreviewButton.blur()
+    }
+}
