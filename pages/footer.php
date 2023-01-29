139310b1caaf8eba7fb64869a9f2fa0e4019cbaa
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2023-01-29 10:47:22 -0800

    Add copyright notice

--- pages/footer.php
+++ pages/footer.php
@@ -5,16 +5,30 @@
 // Only load the page if it's being loaded through the index.php file.
 if (!defined("INDEXED")) exit;
 
-?>
 
-</div><!-- content -->
-<?php listener("afterPageContent"); ?>
 
-<div id="footer">
-<?php listener("beforeFooter");
+echo '</div>';
+listener("afterPageContent");
+echo '<div id="footer">';
+
+listener("beforeFooter");
 echo formatFooter($config["footer"]);
-listener("afterFooter"); ?>
-</div>
-</div><!-- wrapper -->
-</body>
-</html>
+listener("afterFooter"); 
+
+echo '</div>';
+echo '<div id="copyright">';
+if(isset($config["forumTheme"]) and file_exists(dirname(__DIR__,1) . "/themes/" . $config["forumTheme"] . "/icon.svg"))
+{
+    echo '<img width="22" src="' . genURL('themes/' . $config["forumTheme"] . '/icon.svg') . '">';
+}
+echo '© 2022-2023 <a href="https://github.com/tormater/tormater-forum">Tormater Forum</a>';
+echo '</div>';
+listener("afterCopyright");
+
+echo '</div>';
+echo '</div>';
+
+listener("pageEnd");
+
+echo '</body>';
+echo '</html>';
