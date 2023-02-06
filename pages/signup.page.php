d07fc0548a2bd5382cb5a3e9a052efc557a135a2
GigaHacer <94160071+GigaHacer@users.noreply.github.com>
2023-02-05 18:27:01 -0800

    Clean up signup page and make it more configurable

--- pages/signup.page.php
+++ pages/signup.page.php
@@ -1,5 +1,5 @@
 <?php
-//signup.page.php
+// signup.page.php
 // Allows users to create accounts.
 
 // Only load the page if it's being loaded through the index.php file.
@@ -9,79 +9,55 @@ include 'header.php';
 
 echo '<h2>' . $lang["register.Header"] . '</h2>';
 
+// If registration is closed, disallow signups.
+if ($config["registration"] == "closed") {
+    message($lang["register.Closed"]);
+    include "footer.php";
+    exit;
+}
+
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
 	$errors = array();
 	
-	if(isset($_POST['user_name']))
+	if (isset($_POST['user_name']))
 	{
-		$res = $db->query("SELECT salt FROM users WHERE username = '" . $db->real_escape_string($_POST['user_name']) . "'");
-		if($res->num_rows)
-		{
-			$errors[] = $lang["register.UsernameExists"];
-		}
-		if(!checkUsername($_POST['user_name']))
-		{
-			$errors[] = $lang["error.UsernameAlphNum"];
-		}
-		if(strlen($_POST['user_name']) < 3)
-		{
-			$errors[] = $lang["error.UsernameSmall"];
-		}
-		if(strlen($_POST['user_name']) > 24)
-		{
-			$errors[] = $lang["error.UsernameBig"];
-		}
+		validateUsername($_POST["user_name"]);
 	}
 	else
 	{
 		$errors[] = $lang["error.UsernameNull"];
 	}
 
-	if(isset($_POST['user_email']))
+	if (isset($_POST['user_email']))
 	{
-		$res = $db->query("SELECT * FROM users WHERE email ='". $db->real_escape_string($_POST['user_email']) ."'");
-		if($res->num_rows)
-		{
-			$errors[] = $lang["register.EmailExists"];
-		}
-		if(!filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL))
-		{
-			$errors[] = $lang["register.InvalidEmailFormat"];
-		}
+		validateEmail($_POST["user_email"]);
 	}
 	else
 	{
 		$errors[] = $lang["register.EmailEmpty"];
 	}
 
-	if(empty($_POST['user_pass']))
+	if (isset($_POST["user_pass"]))
 	{
-		$errors[] = $lang["register.PasswordEmpty"]; // $lang["error.PassNull"]
+		validatePassword($_POST["user_pass"], $_POST["user_pass_check"]);
 	}
-	if(strlen($_POST['user_pass']) < 6)
-	{
-		$errors[] = $lang["register.PasswordSmall"];
-	}
-	elseif (empty($_POST['user_pass_check']))
-	{
-		$errors[] = $lang["register.ConfirmPasswordEmpty"]; // $lang["error.ConfPassNull"]
-	}
-    	elseif($_POST['user_pass'] != $_POST['user_pass_check'])
-	{
-		$errors[] = $lang["register.ConfirmPasswordWrong"]; // $lang["error.PassConfFail"]
-	}
-	elseif (($config["captchaEnabled"] == true) and ($_SESSION["captcha"] != $_POST["captcha"]))
-    	{
-        	$errors[] = $lang["register.CaptchaWrong"];
-    	}
+    else
+    {
+        $errors[] = $lang["register.PasswordEmpty"];
+    }
 
-    	// Make sure this user hasn't already created the maximum number of accounts.
-    	$altCheck = $db->query("SELECT ip FROM users WHERE ip='" . $db->real_escape_string(hashstring($_SERVER["REMOTE_ADDR"])) . "'");
+	if (($config["captchaEnabled"] == true) and ($_SESSION["captcha"] != $_POST["captcha"]))
+    {
+        $errors[] = $lang["register.CaptchaWrong"];
+    }
 
-    	if ($altCheck->num_rows >= $config["maxAccountsPerIP"]) {
-        	$errors[] = $lang["register.TooManyAccounts"];
-    	}
+    // Make sure this user hasn't already created the maximum number of accounts.
+    $altCheck = $db->query("SELECT ip FROM users WHERE ip='" . $db->real_escape_string(hashstring($_SERVER["REMOTE_ADDR"])) . "'");
+
+    if ($altCheck->num_rows >= $config["maxAccountsPerIP"]) {
+        $errors[] = $lang["register.TooManyAccounts"];
+    }
 
 	if(!empty($errors))
 	{
@@ -95,31 +71,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 	}
 	else
 	{
-		// Construct the query.
-		// First we need to generate our salt in order to salt the password.
-		$salt = random_str(64);
-		$username = $_POST['user_name'];
-		$email = $_POST['user_email'];
-		$password = hashstring($salt . $_POST['user_pass']);
-		$role = "Member";
-		$jointime = time();
-        	$lastactive = time();
-		$color = '1';
-		$ip = hashstring($_SERVER["REMOTE_ADDR"]);
-		$verified = '1';
-		
-		$result = $db->query("INSERT INTO users (username, email, password, role, jointime, lastactive, color, ip, salt, verified) VALUES('" . $db->real_escape_string($username) . "', '" . $db->real_escape_string($email) . "', '" . $db->real_escape_string($password) . "', '" . $db->real_escape_string($role) . "', '" . $db->real_escape_string($jointime) . "', '" . $db->real_escape_string($lastactive) . "', '" . $db->real_escape_string($color) . "', '" . $db->real_escape_string($ip) . "', '" . $db->real_escape_string($salt) . "', '" . $db->real_escape_string($verified) . "')");
-						
-		if(!$result)
-		{
-			echo $lang["error.Database"];
-		}
-		else
-		{
-			printf($lang["register.Success"], genURL("login"));
-			include 'footer.php';
-			exit;
-		}
+        registerUser();
 	}
 }
 
@@ -140,5 +92,113 @@ echo "<div class='forminput'><label></label><input type='submit' class='buttonbi
 </form></div>";
 
 include 'footer.php';
+
+// Validate usernames.
+function validateUsername($username) {
+    global $db, $lang, $errors;
+
+    $res = $db->query("SELECT salt FROM users WHERE username = '" . $db->real_escape_string($username) . "'");
+
+	if($res->num_rows)
+	{
+		$errors[] = $lang["register.UsernameExists"];
+	}
+	if(!checkUsername($username))
+	{
+		$errors[] = $lang["error.UsernameAlphNum"];
+	}
+	if(strlen($username) < 3)
+	{
+		$errors[] = $lang["error.UsernameSmall"];
+	}
+	if(strlen($username) > 24)
+	{
+		$errors[] = $lang["error.UsernameBig"];
+	}
+}
+
+// Validate email addresses.
+function validateEmail($email) {
+    global $db, $lang, $errors;
+
+    $res = $db->query("SELECT * FROM users WHERE email ='". $db->real_escape_string($email) ."'");
+
+	if ($res->num_rows)
+	{
+		$errors[] = $lang["register.EmailExists"];
+	}
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+	{
+		$errors[] = $lang["register.InvalidEmailFormat"];
+	}
+    if (strlen($email) < 1)
+    {
+        $errors[] = $lang["register.EmailShort"];
+    }
+    if (strlen($email) > 254)
+    {
+        $errors[] = $lang["register.EmailLong"];
+    }
+}
+
+// Validate passwords.
+function validatePassword($password, $passwordCheck) {
+    global $lang, $errors;
+    if (strlen($password) < 6)
+	{
+		$errors[] = $lang["register.PasswordSmall"];
+	}
+	if (empty($passwordCheck))
+	{
+		$errors[] = $lang["register.ConfirmPasswordEmpty"];
+	}
+    if ($password != $passwordCheck)
+	{
+		$errors[] = $lang["register.ConfirmPasswordWrong"];
+	}
+}
+
+// Create a new user account.
+function registerUser() {
+    global $db, $lang, $config;
+    // Construct the query.
+	// First we need to generate our salt in order to salt the password.
+	$salt = random_str(64);
+	$username = $_POST['user_name'];
+	$email = $_POST['user_email'];
+	$password = hashstring($salt . $_POST['user_pass']);
+	$role = "Member";
+	$jointime = time();
+    $lastactive = time();
+	$color = '1';
+	$ip = hashstring($_SERVER["REMOTE_ADDR"]);
+    if ($config["registration"] == "open")
+    {
+	    $verified = "1";
+    }
+    elseif ($config["registration"] == "approval")
+    {
+        $verified = "0";
+    }
+		
+	$result = $db->query("INSERT INTO users (username, email, password, role, jointime, lastactive, color, ip, salt, verified) VALUES('" . $db->real_escape_string($username) . "', '" . $db->real_escape_string($email) . "', '" . $db->real_escape_string($password) . "', '" . $db->real_escape_string($role) . "', '" . $db->real_escape_string($jointime) . "', '" . $db->real_escape_string($lastactive) . "', '" . $db->real_escape_string($color) . "', '" . $db->real_escape_string($ip) . "', '" . $db->real_escape_string($salt) . "', '" . $db->real_escape_string($verified) . "')");
+						
+	if (!$result)
+	{
+		echo $lang["error.Database"];
+	}
+	elseif ($config["registration"] == "open")
+	{
+		printf($lang["register.Success"], genURL("login"));
+		include "footer.php";
+		exit;
+	}
+    elseif ($config["registration"] == "approval")
+    {
+        printf($lang["register.Approval"], genURL("login"));
+        include "footer.php";
+        exit;
+    }
+}
 
 ?>
