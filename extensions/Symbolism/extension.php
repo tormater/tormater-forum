afeb13a4ac6d5eda248a02feea822afa9cdcb6d2
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2023-12-02 14:01:03 -0800

    Add more icons

--- extensions/Symbolism/extension.php
+++ extensions/Symbolism/extension.php
@@ -4,7 +4,7 @@ if (!defined("INDEXED")) exit;
 
 function symbolismAddCSS() {
     echo '<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">';
-    echo '<style>fa {font-family:FontAwesome} .threadcontent a:before, #Administrator:before, #Moderator:before, #Member:before, #Suspended:before {font-family:FontAwesome; margin-right:3px}.threadcontent a:before {content:"\f08e";} #Member:before {content:"\f007"} #Moderator:before {content:"\f132"} #Administrator:before {content:"\f0e3"} #Suspended:before {content:"\f05e"}</style>';
+    echo '<style>fa {font-family:FontAwesome} .threadcontent a:before, #Administrator:before, #Moderator:before, #Member:before, #Suspended:before {font-family:FontAwesome; margin-right:3px}.threadcontent a:before {content:"\f08e";} #Member:before {content:"\f007"} #Moderator:before {content:"\f132"} #Administrator:before {content:"\f0e3"} #Suspended:before {content:"\f05e"} .fabbr {text-decoration: none}</style>';
 }
 
 hook("meta","symbolismAddCSS");
@@ -17,17 +17,25 @@ $lang["header.Panel"] = "<fa class='fa-gears'></fa> " . $lang["header.Panel"];
 $lang["header.Logout"] = "<fa class='fa-sign-out'></fa> " . $lang["header.Logout"];
 $lang["header.Login"] = "<fa class='fa-sign-in'></fa> " . $lang["header.Login"];
 $lang["header.Signup"] = "<fa class='fa-external-link'></fa> " . $lang["header.Signup"];
-$lang["label.Sticky"] = "<fa class='fa-thumb-tack'></fa> " . $lang["label.Sticky"];
+$lang["label.Sticky"] = "<fa class='fa-sticky-note-o'></fa> " . $lang["label.Sticky"];
 $lang["label.Locked"] = "<fa class='fa-lock'></fa> " . $lang["label.Locked"];
+$lang["label.Pinned"] = "<fa class='fa-thumb-tack'></fa> " . $lang["label.Pinned"];
 $lang["thread.MoveThreadBtn"] = "<fa class='fa-exchange'></fa> " . $lang["thread.MoveThreadBtn"];
 $lang["thread.DeleteThreadBtn"] = "<fa class='fa-trash-o'></fa> " . $lang["thread.DeleteThreadBtn"];
 $lang["thread.LockThreadBtn"] = "<fa class='fa-lock'></fa> " . $lang["thread.LockThreadBtn"];
-$lang["thread.StickyThreadBtn"] = "<fa class='fa-thumb-tack'></fa> " . $lang["thread.StickyThreadBtn"];
+$lang["thread.StickyThreadBtn"] = "<fa class='fa-sticky-note'></fa> " . $lang["thread.StickyThreadBtn"];
 $lang["thread.UnlockThreadBtn"] = "<fa class='fa-unlock'></fa> " . $lang["thread.UnlockThreadBtn"];
-$lang["thread.UnstickyThreadBtn"] = "<fa class='fa-times'></fa> " . $lang["thread.UnstickyThreadBtn"];
-$lang["post.DeleteBtn"] = "<fa class='fa-trash-o'></fa> " . $lang["post.DeleteBtn"];
-$lang["post.EditBtn"] = "<fa class='fa-pencil'></fa> " . $lang["post.EditBtn"];
-$lang["post.HideBtn"] = "<fa class='fa-eye-slash'></fa> " . $lang["post.HideBtn"];
+$lang["thread.UnstickyThreadBtn"] = "<fa class='fa-sticky-note-o'></fa> " . $lang["thread.UnstickyThreadBtn"];
+$lang["thread.PinThreadBtn"] = "<fa class='fa-thumb-tack'></fa> " . $lang["thread.PinThreadBtn"];
+$lang["thread.UnpinThreadBtn"] = "<fa class='fa-times'></fa> " . $lang["thread.UnpinThreadBtn"];
+
+$lang["post.DeleteBtn"] = "<abbr class='fabbr' title='" . $lang["post.DeleteBtn"] . "'><fa class='fa-trash-o'></fa></abbr>";
+$lang["post.EditBtn"] = "<abbr class='fabbr' title='" . $lang["post.EditBtn"] . "'><fa class='fa-pencil'></fa></abbr>";
+$lang["post.HideBtn"] = "<abbr class='fabbr' title='" . $lang["post.HideBtn"] . "'><fa class='fa-eye-slash'></fa></abbr>";
 $lang["search.Button"] = "<fa class='fa-search'></fa> ". $lang["search.Button"];
+$lang["thread.QuotePost"] = "<abbr class='fabbr' title='" . $lang["thread.QuotePost"] . "'><fa class='fa-quote-left'></fa></abbr>";
+$lang["post.RestoreHiddenBtn"] = "<abbr class='fabbr' title='" . $lang["post.RestoreHiddenBtn"] . "'><fa class='fa-eye'></fa></abbr>";
+
+
 
 ?>
