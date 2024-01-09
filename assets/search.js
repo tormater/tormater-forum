ee514860a759a6979d0d4243fc1fd2efc49d8f7e
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2024-01-09 11:40:23 -0800

    Relative baseURL fix

--- assets/search.js
+++ assets/search.js
@@ -1,5 +1,5 @@
 function search() {
-    let searchPath = new URL(baseURL + '/search/');
+    let searchPath = new URL(baseURL + '/search/', window.location.origin);
     var searchBox = document.getElementById('searchbox');
     window.location.href = searchPath + "?search=" + encodeURIComponent(searchBox.value);
 }
