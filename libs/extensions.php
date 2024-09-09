2d8c8671a44e9312e0f7f6e754c710f1f9abf1e5
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2024-09-09 13:16:07 -0700

    Add extension settings

--- libs/extensions.php
+++ libs/extensions.php
@@ -7,6 +7,11 @@ if (!defined("INDEXED")) exit;
 
 $allExtensions = scandir("extensions");
 
+if (file_exists("config/extension_config.php")) {
+    require "config/extension_config.php";
+}
+saveExtensionSettingsConfig("config/extension_config.php", $extension_config);
+
 foreach ($allExtensions as $e) {
     if ((file_exists("extensions/" . $e . "/manifest.json" )) && (file_exists("extensions/" . $e . "/extension.php" )))
     {
