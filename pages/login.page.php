c2aee963afba542eb6438f2c9f7498715fc5f82b
GigaHacer <94160071+GigaHacer@users.noreply.github.com>
2024-11-16 00:15:41 -0800

    Change password hashing to Bcrypt

--- pages/login.page.php
+++ pages/login.page.php
@@ -7,174 +7,164 @@ if (!defined("INDEXED")) exit;
 
 $next = "";
 if (isset($_GET["next"])) {
-    $next = $_GET["next"];
-    $next = urldecode($next);
+  $next = $_GET["next"];
+  $next = urldecode($next);
 }
 
 if (isset($_SESSION['signed_in']) && ($_SESSION['signed_in'] == true))
 {
-    include 'header.php';
+  include 'header.php';
 			           
-    $data = array(
-	"title" => $lang["login.Header"],
-	"message" => message(sprintf($lang["error.AlreadyLoggedIn"], genURL("logout")), true)
-    );
-    echo $template->render("templates/generic/page_message.html", $data);
-    
+  $data = array(
+  "title" => $lang["login.Header"],
+  "message" => message(sprintf($lang["error.AlreadyLoggedIn"], genURL("logout")), true)
+  );
+  echo $template->render("templates/generic/page_message.html", $data);
 }
 
 else
 {
-	if($_SERVER['REQUEST_METHOD'] != 'POST')
-	{
-	    include 'header.php';
+  if($_SERVER['REQUEST_METHOD'] != 'POST') {
+    include 'header.php';
 	    
-            $data = array(
-                "title" => $lang["login.Header"],
-                "username" => $lang["login.Username"],
-                "password" => $lang["login.Password"],
-                "submit" => $lang["login.Submit"]
-            );
-            echo $template->render("templates/login/login_form.html", $data);
-	}
-	
-	else
-	{
-		$errors = array();
-		
-		if(!isset($_POST['user_name']) || strlen($_POST['user_name']) == 0)
-		{
-			$errors[] = $lang["error.UsernameNull"];
-		}
-		
-		if(!isset($_POST['user_pass']) || strlen($_POST['user_pass']) == 0)
-		{
-			$errors[] = $lang["error.PasswordNull"];
-		}
-		
-		// Delete login records older than 5 minutes.
-        	$db->query("DELETE FROM logins WHERE time<" . time() . "-300");
+    $data = array(
+      "title" => $lang["login.Header"],
+      "username" => $lang["login.Username"],
+      "password" => $lang["login.Password"],
+      "submit" => $lang["login.Submit"]
+    );
+    echo $template->render("templates/login/login_form.html", $data);
+  }
 
-        	// Now check whether too many failed logins have been attempted from this IP.
-        	$logins = $db->query("SELECT * FROM logins WHERE ip='" . $db->real_escape_string(hashstring($_SERVER["REMOTE_ADDR"])) . "'");
+  else {
+    $errors = array();
 
-        	if ($logins->num_rows >= $config["maxLoginAttempts"]) {
-            		$errors[] = $lang["error.TooManyLogins"];
-        	}
+    if(!isset($_POST['user_name']) || strlen($_POST['user_name']) == 0) {
+      $errors[] = $lang["error.UsernameNull"];
+    }
 		
-		if(!empty($errors))
-		{
-		        include 'header.php';
-		        $data = array(
-                	    "title" => $lang["login.Header"],
-                	    "badfields" => $lang["error.BadFields"],
-                	    "errors" => "",
-                	    "go_back" => $lang["error.GoBack"]
-            		);
+    if(!isset($_POST['user_pass']) || strlen($_POST['user_pass']) == 0) {
+      $errors[] = $lang["error.PasswordNull"];
+    }
+		
+    // Delete login records older than 5 minutes.
+    $db->query("DELETE FROM logins WHERE time<" . time() . "-300");
+
+    // Now check whether too many failed logins have been attempted from this IP.
+    $logins = $db->query("SELECT * FROM logins WHERE ip='" . $db->real_escape_string(hashstring($_SERVER["REMOTE_ADDR"])) . "'");
+
+    if ($logins->num_rows >= $config["maxLoginAttempts"]) {
+      $errors[] = $lang["error.TooManyLogins"];
+    }
+		
+    if(!empty($errors)) {
+      include 'header.php';
+      $data = array(
+        "title" => $lang["login.Header"],
+        "badfields" => $lang["error.BadFields"],
+        "errors" => "",
+        "go_back" => $lang["error.GoBack"]
+      );
             		
-			foreach($errors as $key => $value)
-			{
-				$data["errors"] .= $template->render("templates/login/login_error.html", array("error" => $value));
-			}
-			echo $template->render("templates/login/login_errors.html", $data);
-		}
-		else
-		{	
-			$username = $db->real_escape_string($_POST['user_name']);
-            
-            		$resemail = $db->query("SELECT username FROM users WHERE email = '" . $username . "'");
+      foreach($errors as $key => $value) {
+        $data["errors"] .= $template->render("templates/login/login_error.html", array("error" => $value));
+      }
+      echo $template->render("templates/login/login_errors.html", $data);
+    }
+    else {
+      $username = $db->real_escape_string($_POST['user_name']);
 
-            		if(strpos($username, '@') !== "0")
-            		{
-           			while($rowe = $resemail->fetch_assoc()) {
-				$username = $rowe["username"];
-			}
-            	}
+      $resemail = $db->query("SELECT username FROM users WHERE email = '" . $username . "'");
 
-            	$res = $db->query("SELECT salt FROM users WHERE username = '" . $username . "'");
+      if(strpos($username, '@') !== "0") {
+        while($rowe = $resemail->fetch_assoc()) {
+          $username = $rowe["username"];
+        }
+      }
 
-		if(!$res)
-		{
-			$errors[] = $lang["error.Database"];
-		}
-			
-		if (!$res->num_rows)
-		{
-			$errors[] = $lang["error.UsernameWrong"];
-		}
-			
-		while($row = $res->fetch_assoc()) {
-			$salt = $row["salt"];
-		}
+      $res = $db->query("SELECT salt FROM users WHERE username = '" . $username . "'");
+
+      if(!$res) {
+        $errors[] = $lang["error.Database"];
+      }
+
+      if (!$res->num_rows) {
+        $errors[] = $lang["error.UsernameWrong"];
+      }
+
+      while($row = $res->fetch_assoc()) {
+        $salt = $row["salt"];
+      }
+
+      // Now check if the password is correct.
+      $hash = $db->real_escape_string(hashstring($salt . $_POST["user_pass"]));
+
+      $result = $db->query("SELECT userid, username, role, password, verified FROM users WHERE username = '" . $username . "'");
+
+      if(!$result) {
+        $errors[] = $lang["error.Database"];
+      }
+      else {
+        if (!$result->num_rows) {
+          $errors[] = $lang["error.UsernameWrong"];
+        }
+        while($row = $result->fetch_assoc()) {
+          $userid = $row["userid"];
+          $username = $row["username"];
+          $role = $row["role"];
+          if ($row["verified"] == "0") {
+            $errors[] = $lang["error.NeedsApproval"];
+          }
+          // If the password doesn't match the hash in the database, we need some additional checks.
+          if ($row["password"] != $hash) {
+            // Check if the hash is a bcrypt hash.
+            if (!password_verify($_POST["user_pass"], $row["password"])) {
+              $errors[] = $lang["error.PasswordWrong"];
+            }
+          }
+          // If it does, we want to upgrade it to a bcrypt hash.
+          else {
+            $db->query("UPDATE users SET password='" . $db->real_escape_string(password_hash($_POST["user_pass"], PASSWORD_DEFAULT)) . "' WHERE userid='" . $db->real_escape_string($userid) . "'");
+          }
+        }
+      }
+
+      if(!empty($errors)) {
+        // Log the failed login attempt.
+        $db->query("INSERT INTO logins (ip, time) VALUES ('" . $db->real_escape_string(hashstring($_SERVER["REMOTE_ADDR"])) . "', '" . time() . "')");
 				
-			// Now check if the password is correct.
-		$hash = $db->real_escape_string(hashstring($salt . $_POST["user_pass"]));
-			
-
-		$result = $db->query("SELECT userid, username, role, verified FROM users WHERE username = '" . $username . "' AND password = '" . $hash . "'");
-			
-			
-		if(!$result)
-		{
-			$errors[] = $lang["error.Database"];
-		}
-			
-		else
-		{
-		    if (!$result->num_rows)
-		    {
-			$errors[] = $lang["error.PasswordWrong"];
-		    }
-                    while($row = $result->fetch_assoc())
-                    {
-                    	if ($row["verified"] == "0")
-                        {
-                            $errors[] = $lang["error.NeedsApproval"];
-                        }
-				
-			else
-			{
-			    $_SESSION['signed_in'] = true;
-				
-			    $_SESSION['userid'] = $row['userid'];
-			    $_SESSION['username'] = $row['username'];
-			    $_SESSION['role'] = $row['role'];
+        include 'header.php';
+        $data = array(
+          "title" => $lang["login.Header"],
+          "badfields" => $lang["error.BadFields"],
+          "errors" => "",
+          "go_back" => $lang["error.GoBack"]
+        );
+            		
+        foreach($errors as $key => $value) {
+          $data["errors"] .= $template->render("templates/login/login_error.html", array("error" => $value));
+        }
+        echo $template->render("templates/login/login_errors.html", $data);
+      }
+      else {
+        $_SESSION['signed_in'] = true;
+        $_SESSION['userid'] = $userid;
+        $_SESSION['username'] = $username;
+        $_SESSION['role'] = $role;
 					
-					    // Write the IP to the database.
-			    $db->query("UPDATE users SET ip='" . $db->real_escape_string(hashstring($_SERVER["REMOTE_ADDR"])) . "' WHERE userid='" . $_SESSION["userid"] . "'");
-			    header("Refresh:1; url=" . genURL($next));
-			    include 'header.php';
+        // Write the IP to the database.
+        $db->query("UPDATE users SET ip='" . $db->real_escape_string(hashstring($_SERVER["REMOTE_ADDR"])) . "' WHERE userid='" . $_SESSION["userid"] . "'");
+        header("Refresh:1; url=" . genURL($next));
+        include 'header.php';
 			           
-			    $data = array(
-				"title" => $lang["login.Header"],
-				"message" => message(sprintf($lang["login.Welcome"], htmlspecialchars($_SESSION['username']), genURL($next)),true)
-			    );
-			    echo $template->render("templates/generic/page_message.html", $data);
-                            }
-			}
-		    }
-
-	            if(!empty($errors))
-	            {
-			// Log the failed login attempt.
-                	$db->query("INSERT INTO logins (ip, time) VALUES ('" . $db->real_escape_string(hashstring($_SERVER["REMOTE_ADDR"])) . "', '" . time() . "')");
-				
-			include 'header.php';
-		        $data = array(
-                	    "title" => $lang["login.Header"],
-                	    "badfields" => $lang["error.BadFields"],
-                	    "errors" => "",
-                	    "go_back" => $lang["error.GoBack"]
-            		);
-            		
-			foreach($errors as $key => $value)
-			{
-				$data["errors"] .= $template->render("templates/login/login_error.html", array("error" => $value));
-			}
-			echo $template->render("templates/login/login_errors.html", $data);
-	             }
-		}
-	}
+        $data = array(
+          "title" => $lang["login.Header"],
+          "message" => message(sprintf($lang["login.Welcome"], htmlspecialchars($_SESSION['username']), genURL($next)),true)
+        );
+        echo $template->render("templates/generic/page_message.html", $data);
+      }
+    }
+  }
 }
 
 include 'footer.php';
