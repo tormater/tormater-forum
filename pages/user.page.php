123b0f903ff4a7c7469c184ec9e972e37671c643
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2024-12-26 00:51:56 -0800

    Remove moderator's ability to take away admin and moderator's roles

--- pages/user.page.php
+++ pages/user.page.php
@@ -10,47 +10,22 @@ $result = $db->query("SELECT * FROM users WHERE userid='" . $db->real_escape_str
 
 if (!$result)
 {
-	message($lang["user.FaildFindUser"]);
+    message($lang["user.FaildFindUser"]);
 }
 
 else
 {
     
-	if ($result->num_rows == 0)
-	{
-		message($lang["user.NoSuchUser"]);
-	}
-	
-	else
-	{
-		if ((isset($_POST["role"])) and (($_SESSION["role"] == "Administrator") or ($_SESSION["role"] == "Moderator")))
-		{
-            if (is_numeric($q2) and ($_SESSION["userid"] != $q2) and ($config["mainAdmin"] != $q2)) {
-                if (($_SESSION["role"] == "Moderator") and ($_POST["role"] != "Member") and ($_POST["role"] != "Suspended")) {
-                    // Do nothing
-                }
-                else {
-			        $setrole = $db->query("UPDATE users SET role='" . $db->real_escape_string($_POST["role"]) . "' WHERE userid='" . $db->real_escape_string($q2) . "'");
-			
-			        if (!$setrole)
-			        {
-				        echo $lang["user.FaildChangeRole"];
-			        }
-			
-			        refresh(0);
-                }
-            }
-		}
-        elseif ((isset($_POST["removeAvatar"])) and (($_SESSION["role"] == "Administrator") or ($_SESSION["role"] == "Moderator")))
+    if ($result->num_rows == 0)
+    {
+        message($lang["user.NoSuchUser"]);
+    }
+    
+    else
+    {
+        while ($row = $result->fetch_assoc())
         {
-            removeAvatar($q2);
-
-            refresh(0);
-        }
-
-		while ($row = $result->fetch_assoc())
-		{
-      		if ($row["deleted"] == "1") $username = $lang["user.Deleted"] . $row["userid"];
+            if ($row["deleted"] == "1") $username = $lang["user.Deleted"] . $row["userid"];
             else $username = $row["username"];
             
             $userid = $row["userid"];
@@ -59,32 +34,54 @@ else
             $verified = $row["verified"];
             $lastactive = $row["lastactive"];
             $jointime = $row["jointime"];
-			$deleted = $row["deleted"];
-			$avatar = $row["avatar"];
-			$avatarTime = $row["avataruploadtime"];
+            $deleted = $row["deleted"];
+            $avatar = $row["avatar"];
+            $avatarTime = $row["avataruploadtime"];
+        }
+        
+        if ((isset($_POST["role"])) and (($_SESSION["role"] == "Administrator") or ($_SESSION["role"] == "Moderator")))
+        {
+            if (is_numeric($q2) and ($_SESSION["userid"] != $q2) and ($config["mainAdmin"] != $q2)) {
+                if ($_SESSION["role"] == "Administrator" || ($_SESSION["role"] == "Moderator" and ($_POST["role"] == "Member" or $_POST["role"] == "Suspended") and ($role == "Member" or $role == "Suspended"))) {
+                    $setrole = $db->query("UPDATE users SET role='" . $db->real_escape_string($_POST["role"]) . "' WHERE userid='" . $db->real_escape_string($q2) . "'");
+            
+                    if (!$setrole)
+                    {
+                        echo $lang["user.FaildChangeRole"];
+                    }
+                                
+                    refresh(0);
+                }
+            }
+        }
+        elseif ((isset($_POST["removeAvatar"])) and (($_SESSION["role"] == "Administrator") or ($_SESSION["role"] == "Moderator")))
+        {
+            removeAvatar($q2);
+
+            refresh(0);
         }
 
         include "header.php";
-		
+        
         // Get the profile's status
-	if ($verified == "1") $verified = $lang["user.VerifiedYes"];
-	else $verified = $lang["user.VerifiedNo"];
+    if ($verified == "1") $verified = $lang["user.VerifiedYes"];
+    else $verified = $lang["user.VerifiedNo"];
 
-	$delClass = "";
-		
-	if ($deleted == "1") 
+    $delClass = "";
+        
+    if ($deleted == "1") 
         {
             $deleted = $lang["user.DeletedYes"];
             $delClass = " deleteduser";
         }
 
-	else $deleted = $lang["user.DeletedNo"];
-		
-	if ($avatar == "none") $uAvatar = "";
+    else $deleted = $lang["user.DeletedNo"];
+        
+    if ($avatar == "none") $uAvatar = "";
         else $uAvatar = '<img class="avatar" src="' . genURL("avatars/" . $userid . "." . $avatar . "?t=" . $avatarTime) . '">';
-			
+            
         echo '<h2>'.$lang["user.ViewingProfile"].' "' . htmlspecialchars($username) . '"</h2>';
-        echo '<div class="post' . $delClass . '"><div class="usertop" postcolor="' . htmlspecialchars($color) . '">';	
+        echo '<div class="post' . $delClass . '"><div class="usertop" postcolor="' . htmlspecialchars($color) . '">';    
 
         drawUserProfile($userid, 1);
 
@@ -93,23 +90,23 @@ else
         }
 
         echo "</div></div>";
-		
+        
         // Get user statistics
 
-		$posts = $db->query("SELECT 1 FROM posts WHERE user='" . $db->real_escape_string($q2) . "'");
+        $posts = $db->query("SELECT 1 FROM posts WHERE user='" . $db->real_escape_string($q2) . "'");
         $uposts = mysqli_num_rows($posts);
-				
-		$threads = $db->query("SELECT 1 FROM threads WHERE startuser='" . $db->real_escape_string($q2) . "'");
-		$uthreads = mysqli_num_rows($threads);
+                
+        $threads = $db->query("SELECT 1 FROM threads WHERE startuser='" . $db->real_escape_string($q2) . "'");
+        $uthreads = mysqli_num_rows($threads);
 
-		echo '<div class="userbottom">
-            		<h3>' . $lang["user.UserInformation"] . '</h3>
-			<span class="userstat"><label class="shortlabel">'.$lang["user.TitleRegistered"].'</label><a title="' . date('m-d-Y h:i:s A', $jointime) . '">' . relativeTime($jointime) . '</a></span>
-			<span class="userstat"><label class="shortlabel">'.$lang["user.TitleLastActive"].'</label><a title="' . date('m-d-Y h:i:s A', $lastactive) . '">' . relativeTime($lastactive) . '</a></span>
-			<span class="userstat"><label class="shortlabel">'.$lang["user.TitlePosts"].'</label>' . $uposts . '</span>
-			<span class="userstat"><label class="shortlabel">'.$lang["user.TitleThreads"].'</label>' . $uthreads . '</span>
-			<span class="userstat"><label class="shortlabel">'.$lang["user.TitleVerified"].'</label>' . $verified . '</span>';
-	}
+        echo '<div class="userbottom">
+                    <h3>' . $lang["user.UserInformation"] . '</h3>
+            <span class="userstat"><label class="shortlabel">'.$lang["user.TitleRegistered"].'</label><a title="' . date('m-d-Y h:i:s A', $jointime) . '">' . relativeTime($jointime) . '</a></span>
+            <span class="userstat"><label class="shortlabel">'.$lang["user.TitleLastActive"].'</label><a title="' . date('m-d-Y h:i:s A', $lastactive) . '">' . relativeTime($lastactive) . '</a></span>
+            <span class="userstat"><label class="shortlabel">'.$lang["user.TitlePosts"].'</label>' . $uposts . '</span>
+            <span class="userstat"><label class="shortlabel">'.$lang["user.TitleThreads"].'</label>' . $uthreads . '</span>
+            <span class="userstat"><label class="shortlabel">'.$lang["user.TitleVerified"].'</label>' . $verified . '</span>';
+    }
 
 echo '</div></div>';
 
@@ -147,21 +144,21 @@ $posts = $db->query("SELECT * FROM posts WHERE user='" . $db->real_escape_string
 
 if($posts->num_rows == 0)
 {
-	message($lang["thread.NoPosts"]);
+    message($lang["thread.NoPosts"]);
 }
-	
+    
 else
 {
-	while($row = $posts->fetch_assoc())
-	{
-		$userinfo = $db->query("SELECT * FROM users WHERE userid='" . $row["user"] . "'");
-			
-		while($u = $userinfo->fetch_assoc())
-		{
-			echo '<div class="userpost">' . formatPost($row["content"]) . '</div>';
+    while($row = $posts->fetch_assoc())
+    {
+        $userinfo = $db->query("SELECT * FROM users WHERE userid='" . $row["user"] . "'");
+            
+        while($u = $userinfo->fetch_assoc())
+        {
+            echo '<div class="userpost">' . formatPost($row["content"]) . '</div>';
             echo "<span class='postdate' title='" . date('m-d-Y h:i:s A', $row["timestamp"]) . "'>" . relativeTime($row["timestamp"]) . "</span> - <a class='userpostlink' href='" . genURL("thread/" . $row["thread"]) . "'>" . $lang["user.ViewThread"] . "</a>";
-		}
-	}
+        }
+    }
 }
 echo "</div></div>";
 }
@@ -169,8 +166,8 @@ echo "</div></div>";
 // If the viewing user is logged in, update their last action.
 if (isset($_SESSION['signed_in']) && ($_SESSION['signed_in'] == true))
 {
-	$action = $lang["action.Generic"]. '<a href="' . genURL('user/' . $userid) . '/">' . htmlspecialchars($username) . $lang["action.UserProfile"] . '</a>';
-	update_last_action($action);
+    $action = $lang["action.Generic"]. '<a href="' . genURL('user/' . $userid) . '/">' . htmlspecialchars($username) . $lang["action.UserProfile"] . '</a>';
+    update_last_action($action);
 }
 
 include "footer.php";
