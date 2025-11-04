4053b21ef388578e62ab8b749beeac4ac4690ee0
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2025-11-04 14:47:35 -0800

    Fix PHP warning

--- pages/panelsettings.page.php
+++ pages/panelsettings.page.php
@@ -42,6 +42,7 @@ if($_SERVER['REQUEST_METHOD'] != 'POST')
     // Display the change forum color form.
     $presetColor = "#000000";
     $defaultchecked = "";
+    $newchecked = "";
     if ($config["forumColor"] != null)
     {
         $presetColor = $config["forumColor"];
