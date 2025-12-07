d2f07e4036100b4c98bb25e547563fd4cc9fbd2e
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2025-12-07 15:33:41 -0800

    Update permission.php

--- libs/permission.php
+++ libs/permission.php
@@ -15,12 +15,16 @@ define('PERM_VIEW_AUDIT', 64);
 define('PERM_CREATE_THREAD', 128);
 define('PERM_CREATE_POST', 256);
 define('PERM_VIEW_USERLIST', 512);
+define('PERM_VIEW_USER', 1024);
+define('PERM_VIEW_THREAD', 2048);
+define('PERM_VIEW_CATEGORY', 4096);
+define('PERM_EDIT_PROFILE', 8192);
 
 $permissions = array(
-    "Administrator" => PERM_EDIT_POST|PERM_EDIT_THREAD|PERM_EDIT_USER|PERM_EDIT_CATEGORY|PERM_EDIT_FORUM|PERM_VIEW_AUDIT|PERM_CREATE_THREAD|PERM_CREATE_POST|PERM_VIEW_USERLIST,
-    "Moderator" => PERM_EDIT_POST|PERM_EDIT_THREAD|PERM_EDIT_USER|PERM_VIEW_AUDIT|PERM_CREATE_THREAD|PERM_CREATE_POST|PERM_VIEW_USERLIST,
-    "Member" => PERM_CREATE_THREAD|PERM_CREATE_POST|PERM_VIEW_USERLIST,
-    "Guest" => PERM_VIEW_USERLIST,
+    "Administrator" => PERM_EDIT_POST|PERM_EDIT_THREAD|PERM_EDIT_USER|PERM_EDIT_CATEGORY|PERM_EDIT_FORUM|PERM_VIEW_AUDIT|PERM_CREATE_THREAD|PERM_CREATE_POST|PERM_VIEW_USERLIST|PERM_VIEW_USER|PERM_VIEW_THREAD|PERM_VIEW_CATEGORY|PERM_EDIT_PROFILE,
+    "Moderator" => PERM_EDIT_POST|PERM_EDIT_THREAD|PERM_EDIT_USER|PERM_VIEW_AUDIT|PERM_CREATE_THREAD|PERM_CREATE_POST|PERM_VIEW_USERLIST|PERM_VIEW_USER|PERM_VIEW_THREAD|PERM_VIEW_CATEGORY|PERM_EDIT_PROFILE,
+    "Member" => PERM_CREATE_THREAD|PERM_CREATE_POST|PERM_VIEW_USERLIST|PERM_VIEW_USER|PERM_VIEW_THREAD|PERM_VIEW_CATEGORY|PERM_EDIT_PROFILE,
+    "Guest" => PERM_VIEW_USERLIST|PERM_VIEW_USER|PERM_VIEW_THREAD|PERM_VIEW_CATEGORY,
 );
 
 if ($config["userlistMembersOnly"]) {
@@ -29,12 +33,18 @@ if ($config["userlistMembersOnly"]) {
 
 listener("afterDefaultPerms");
 
-function get_role_permissions() {   
-    global $permissions;
-    
+function get_role_from_session() {
     if (isset($_SESSION["role"])) $role = $_SESSION["role"];
     else if (isset($_SESSION['signed_in']) && $_SESSION['signed_in']) $role = "Member";
     else $role = "Guest";
+    return $role;
+}
+
+function get_role_permissions($r = "") {   
+    global $permissions;
+    
+    if ($r != "") $role = $r;
+    else $role = get_role_from_session();
     
     return $permissions[$role];
 }
