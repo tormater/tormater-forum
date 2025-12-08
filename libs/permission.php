5e55b20aa1c8012c5f4f44df987c23d270539534
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2025-12-07 16:36:40 -0800

    Update permission.php

--- libs/permission.php
+++ libs/permission.php
@@ -19,12 +19,14 @@ define('PERM_VIEW_USER', 1024);
 define('PERM_VIEW_THREAD', 2048);
 define('PERM_VIEW_CATEGORY', 4096);
 define('PERM_EDIT_PROFILE', 8192);
+define('PERM_IGNORE_THREAD_LABELS', 16384);
 
 $permissions = array(
-    "Administrator" => PERM_EDIT_POST|PERM_EDIT_THREAD|PERM_EDIT_USER|PERM_EDIT_CATEGORY|PERM_EDIT_FORUM|PERM_VIEW_AUDIT|PERM_CREATE_THREAD|PERM_CREATE_POST|PERM_VIEW_USERLIST|PERM_VIEW_USER|PERM_VIEW_THREAD|PERM_VIEW_CATEGORY|PERM_EDIT_PROFILE,
-    "Moderator" => PERM_EDIT_POST|PERM_EDIT_THREAD|PERM_EDIT_USER|PERM_VIEW_AUDIT|PERM_CREATE_THREAD|PERM_CREATE_POST|PERM_VIEW_USERLIST|PERM_VIEW_USER|PERM_VIEW_THREAD|PERM_VIEW_CATEGORY|PERM_EDIT_PROFILE,
+    "Administrator" => PERM_EDIT_POST|PERM_EDIT_THREAD|PERM_EDIT_USER|PERM_EDIT_CATEGORY|PERM_EDIT_FORUM|PERM_VIEW_AUDIT|PERM_CREATE_THREAD|PERM_CREATE_POST|PERM_VIEW_USERLIST|PERM_VIEW_USER|PERM_VIEW_THREAD|PERM_VIEW_CATEGORY|PERM_EDIT_PROFILE|PERM_IGNORE_THREAD_LABELS,
+    "Moderator" => PERM_EDIT_POST|PERM_EDIT_THREAD|PERM_EDIT_USER|PERM_VIEW_AUDIT|PERM_CREATE_THREAD|PERM_CREATE_POST|PERM_VIEW_USERLIST|PERM_VIEW_USER|PERM_VIEW_THREAD|PERM_VIEW_CATEGORY|PERM_EDIT_PROFILE|PERM_IGNORE_THREAD_LABELS,
     "Member" => PERM_CREATE_THREAD|PERM_CREATE_POST|PERM_VIEW_USERLIST|PERM_VIEW_USER|PERM_VIEW_THREAD|PERM_VIEW_CATEGORY|PERM_EDIT_PROFILE,
     "Guest" => PERM_VIEW_USERLIST|PERM_VIEW_USER|PERM_VIEW_THREAD|PERM_VIEW_CATEGORY,
+    "Suspended" => PERM_VIEW_USERLIST|PERM_VIEW_USER|PERM_VIEW_THREAD|PERM_VIEW_CATEGORY,
 );
 
 if ($config["userlistMembersOnly"]) {
@@ -34,10 +36,9 @@ if ($config["userlistMembersOnly"]) {
 listener("afterDefaultPerms");
 
 function get_role_from_session() {
-    if (isset($_SESSION["role"])) $role = $_SESSION["role"];
-    else if (isset($_SESSION['signed_in']) && $_SESSION['signed_in']) $role = "Member";
-    else $role = "Guest";
-    return $role;
+    if (isset($_SESSION["role"])) return $_SESSION["role"];
+    else if (isset($_SESSION['signed_in']) && $_SESSION['signed_in']) return "Member";
+    else return "Guest";
 }
 
 function get_role_permissions($r = "") {   
@@ -47,4 +48,16 @@ function get_role_permissions($r = "") {
     else $role = get_role_from_session();
     
     return $permissions[$role];
+}
+
+function get_roles() {
+    global $permissions;
+    return array_diff(array_keys($permissions),array("Guest"));
+}
+
+function get_changeable_roles($r) {
+    if ($r == "Guest") return NULL;
+    if (!(get_role_permissions($r) & PERM_EDIT_USER)) return NULL;
+    if (get_role_permissions($r) & PERM_EDIT_FORUM) return get_roles();
+    return array_slice(get_roles(),array_search($r,get_roles())+1);
 }
