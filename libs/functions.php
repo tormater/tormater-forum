20b8236e77a148976e9d2204191e32b22257ddf8
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2025-12-17 21:59:09 -0800

    Update functions.php

--- libs/functions.php
+++ libs/functions.php
@@ -724,7 +724,7 @@ function drawNavigation() {
     }
     if ($q1 == "thread")
     {
-        if (($GLOBALS["threadExists"] == true) and (($GLOBALS["draft"] == 0) or ($_SESSION["userid"] == $GLOBALS["startuser"]) or (($_SESSION["role"] == "Moderator") or ($_SESSION["role"] == "Administrator")))) {
+        if ((isset($GLOBALS["title"]) and isset($GLOBALS["categoryID"])) and (($GLOBALS["draft"] == 0) or ($_SESSION["userid"] == $GLOBALS["startuser"]) or (($_SESSION["role"] == "Moderator") or ($_SESSION["role"] == "Administrator")))) {
         // Get the category information.
             $categoryDB = $db->query("SELECT * FROM categories WHERE categoryid='" . $db->real_escape_string($GLOBALS["categoryID"]) . "'");
 
