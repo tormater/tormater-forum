528c5025bf11996223cb5e0dee51722c2acffb90
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2026-08-20 14:52:27 -0700

    Tighten username validation, use templates & move functions

--- pages/signup.page.php
+++ pages/signup.page.php
@@ -5,189 +5,106 @@
 // Only load the page if it's being loaded through the index.php file.
 if (!defined("INDEXED")) exit;
 
-// If registration is closed, disallow signups.
 if ($config["registration"] == "closed") {
-  include 'header.php';
-  echo '<h2>' . $lang["register.Header"] . '</h2>';
-  message($lang["register.Closed"]);
-  include "footer.php";
-  exit;
+    include 'header.php';
+    echo '<h2>' . $lang["register.Header"] . '</h2>';
+    message($lang["register.Closed"]);
+    include "footer.php";
+    exit;
 }
-if (isset($_SESSION["signed_in"]) && $_SESSION["signed_in"] == true) {
-  include 'header.php';
-  echo '<h2>' . $lang["register.Header"] . '</h2>';
-  message($lang["nav.AdminsOnly"]);
-  include "footer.php";
-  exit;
-}
-
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-  $errors = array();
-	
-  if (isset($_POST['user_name'])) {
-    validateUsername($_POST["user_name"]);
-  }
-  else {
-    $errors[] = $lang["error.UsernameNull"];
-  }
-
-  if (isset($_POST['user_email'])) {
-    validateEmail($_POST["user_email"]);
-  }
-  else {
-    $errors[] = $lang["register.EmailEmpty"];
-  }
-
-  if (isset($_POST["user_pass"])) {
-    validatePassword($_POST["user_pass"], $_POST["user_pass_check"]);
-  }
-  else {
-    $errors[] = $lang["register.PasswordEmpty"];
-  }
-
-  if (($config["captchaEnabled"] == true) and ($_SESSION["captcha"] != $_POST["captcha"])) {
-    $errors[] = $lang["register.CaptchaWrong"];
-  }
-
-  // Make sure this user hasn't already created the maximum number of accounts.
-  $altCheck = $db->query("SELECT ip FROM users WHERE ip='" . $db->real_escape_string(hashstring($_SERVER["REMOTE_ADDR"])) . "'");
-    
-  if ($altCheck->num_rows >= $config["maxAccountsPerIP"]) {
-    $errors[] = $lang["register.TooManyAccounts"];
-  }
-
-  if(count($errors) <= 0) {
-    registerUser();
-  }
+if (get_role_from_session() != "Guest") {
+    include 'header.php';
+    echo '<h2>' . $lang["register.Header"] . '</h2>';
+    message($lang["nav.AdminsOnly"]);
+    include "footer.php";
+    exit;
 }
 
 include 'header.php';
-echo '<h2>' . $lang["register.Header"] . '</h2>';
 
-if (isset($errors) and count($errors)) {
-  echo $lang["error.BadFields"];
-  echo '<ul>';
-  foreach($errors as $key => $value) {
-    echo '<li>' . $value . '</li>';
-  }
-  echo '</ul>';
+$data = array(
+    "header" => $lang["register.Header"],
+    "error" => "",
+    "username_desc" => $lang["register.UsernameDesc"],
+    "username_label" => $lang["register.Username"],
+    "username_maxlength" => $config['maxUsernameLength'],
+    "username" => @htmlspecialchars(@$_POST["user_name"]),
+    "email_desc" => $lang["register.EmailDesc"],
+    "email_label" => $lang["register.Email"],
+    "email" => @htmlspecialchars($_POST["user_email"]),
+    "password_desc" => sprintf($lang["register.PasswordDesc"], "6"),
+    "password_label" => $lang["register.Password"],
+    "password" => @htmlspecialchars($_POST["user_pass"]),
+    "confirm_password_label" => $lang["register.PasswordConf"],
+    "confirm_password" => @htmlspecialchars($_POST["user_pass_check"]),
+    "captcha" => "",
+    "submit" => $lang["register.Submit"]
+);
+
+function registerUser(&$data) {
+    global $db, $lang, $config;
+    
+    if ($config["captchaEnabled"] == true and !checkCaptcha($_SESSION["captcha"],$_POST["captcha"])) {
+        $data["error"] .= message($lang["register.CaptchaWrong"],true);
+        return;
+    }
+    
+    $altCheck = $db->query("SELECT ip FROM users WHERE ip='" . $db->real_escape_string(hashstring($_SERVER["REMOTE_ADDR"])) . "'");
+    if ($altCheck->num_rows >= $config["maxAccountsPerIP"]) {
+        $data["error"] .= message($lang["register.TooManyAccounts"],true);
+        return;
+    }   
+    
+    $data["error"] .= validateUsername(@trim(@$_POST["user_name"]));
+    $data["error"] .= validateEmail(@$_POST["user_email"]);
+    $data["error"] .= validatePassword(@$_POST["user_pass"], @$_POST["user_pass_check"]);
+    if (strlen($data["error"])) return;
+    
+    $salt = "";
+    $username = trim($_POST['user_name']);
+    $email = $_POST['user_email'];
+    $password = password_hash($_POST['user_pass'], PASSWORD_DEFAULT);
+    $role = "Member";
+    $jointime = time(); $lastactive = time();
+    $color = '1';
+    $ip = hashstring($_SERVER["REMOTE_ADDR"]);
+    if ($config["registration"] == "open") $verified = "1";
+    else $verified = "0";
+		
+    $result = $db->query("INSERT INTO users (username, email, password, role, jointime, lastactive, color, ip, salt, verified) VALUES('" . $db->real_escape_string($username) . "', '" . $db->real_escape_string($email) . "', '" . $db->real_escape_string($password) . "', '" . $db->real_escape_string($role) . "', '" . $db->real_escape_string($jointime) . "', '" . $db->real_escape_string($lastactive) . "', '" . $db->real_escape_string($color) . "', '" . $db->real_escape_string($ip) . "', '" . $db->real_escape_string($salt) . "', '" . $db->real_escape_string($verified) . "')");
+						
+    if (!$result) {
+        $data["error"] .= $lang["error.Database"];
+        return;
+    }
+    if ($config["registration"] == "open") {
+        $result = $db->query("SELECT userid, username, role FROM users WHERE username='" . $db->real_escape_string($username) . "'");
+
+        $row = $result->fetch_assoc();
+        $_SESSION['signed_in'] = true;
+        $_SESSION['userid'] = $row["userid"];
+        $_SESSION['username'] = $row["username"];
+        $_SESSION['role'] = $row["role"];
+        redirect("userpanel");
+    }
+    else {
+        echo '<h2>' . $lang["register.Header"] . '</h2>';
+        printf($lang["register.Approval"], genURL("login"));
+        include "footer.php";
+        exit;
+    }
 }
 
-echo "<div class='formcontainer'><form method='post' action=''>
-<div class='forminput'><label><abbr title='" . $lang["register.UsernameDesc"] . "'>" . $lang["register.Username"] . "</abbr></label><input type='text' name='user_name' autocomplete='username' maxlength='". $config['maxUsernameLength'] . "' value='" . @htmlspecialchars(@$_POST["user_name"]) . "' /></div>
-<div class='forminput'><label><abbr title='" . $lang["register.EmailDesc"] . "'>" . $lang["register.Email"] . "</abbr></label><input type='email' name='user_email' maxlength='254' value='" . @htmlspecialchars($_POST["user_email"]) . "'></div>
-<div class='forminput'><label><abbr title='" . sprintf($lang["register.PasswordDesc"], "6") . "'>" . $lang["register.Password"] . "</abbr></label><input type='password' name='user_pass' value='" . @htmlspecialchars($_POST["user_pass"]) . "'></div>
-<div class='forminput'><label>" . $lang["register.PasswordConf"] . "</label><input type='password' name='user_pass_check' value='" . @htmlspecialchars($_POST["user_pass_check"]) . "'></div>";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') registerUser($data);
+
 if ($config["captchaEnabled"] == true) {
-  echo "<br/><div class='forminput'><label>" . $lang["register.Captcha"] . "</label><input type='text' name='captcha'></div>";
-  echo "<small class='captchalabel'>" . $lang["register.CaptchaHint"] . "</small>";
-  $_SESSION["captcha"] = generateCaptcha($config["captchaLength"]);
+    $data["captcha"] .= "<br/><div class='forminput'><label>" . $lang["register.Captcha"] . "</label><input type='text' name='captcha'></div>";
+    $data["captcha"] .=  "<small class='captchalabel'>" . $lang["register.CaptchaHint"] . "</small>";
+    $_SESSION["captcha"] = randomCaptcha($config["captchaLength"]);
+    $data["captcha"] .= generateCaptcha($_SESSION["captcha"]);
 }
-echo "<div class='forminput'><label></label><input type='submit' class='buttonbig' value='" . $lang["register.Submit"] . "' /></div>
-</form></div>";
+
+echo $template->render("templates/signup/signup_form.html",$data);
 
 include 'footer.php';
-
-// Validate usernames.
-function validateUsername($username) {
-  global $db, $lang, $errors, $config;
-
-  $res = $db->query("SELECT salt FROM users WHERE username = '" . $db->real_escape_string($username) . "'");
-
-  if($res->num_rows) {
-    $errors[] = $lang["register.UsernameExists"];
-  }
-  if(!checkUsername(htmlspecialchars_decode($username))) {
-    $errors[] = $lang["error.UsernameAlphNum"];
-  }
-  if(strlen($username) < 3) {
-    $errors[] = $lang["error.UsernameSmall"];
-  }
-  if(strlen($username) > $config['maxUsernameLength']) {
-    $errors[] = $lang["error.UsernameBig"];
-  }
-}
-
-// Validate email addresses.
-function validateEmail($email) {
-  global $db, $lang, $errors;
-
-  $res = $db->query("SELECT * FROM users WHERE email ='". $db->real_escape_string($email) ."'");
-
-  if ($res->num_rows) {
-    $errors[] = $lang["register.EmailExists"];
-  }
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $errors[] = $lang["register.InvalidEmailFormat"];
-  }
-  if (strlen($email) < 1) {
-    $errors[] = $lang["register.EmailShort"];
-  }
-  if (strlen($email) > 254) {
-    $errors[] = $lang["register.EmailLong"];
-  }
-}
-
-// Validate passwords.
-function validatePassword($password, $passwordCheck) {
-  global $lang, $errors;
-  if (strlen($password) < 6) {
-    $errors[] = $lang["register.PasswordSmall"];
-  }
-  if (empty($passwordCheck)) {
-    $errors[] = $lang["register.ConfirmPasswordEmpty"];
-  }
-  if ($password != $passwordCheck) {
-    $errors[] = $lang["register.ConfirmPasswordWrong"];
-  }
-}
-
-// Create a new user account.
-function registerUser() {
-  global $db, $lang, $config;
-  // Construct the query.
-  // First we need to generate our salt in order to salt the password.
-  $salt = "";
-  $username = $_POST['user_name'];
-  $email = $_POST['user_email'];
-  $password = password_hash($_POST['user_pass'], PASSWORD_DEFAULT);
-  $role = "Member";
-  $jointime = time();
-  $lastactive = time();
-  $color = '1';
-  $ip = hashstring($_SERVER["REMOTE_ADDR"]);
-  if ($config["registration"] == "open") {
-    $verified = "1";
-  }
-  elseif ($config["registration"] == "approval") {
-    $verified = "0";
-  }
-		
-  $result = $db->query("INSERT INTO users (username, email, password, role, jointime, lastactive, color, ip, salt, verified) VALUES('" . $db->real_escape_string($username) . "', '" . $db->real_escape_string($email) . "', '" . $db->real_escape_string($password) . "', '" . $db->real_escape_string($role) . "', '" . $db->real_escape_string($jointime) . "', '" . $db->real_escape_string($lastactive) . "', '" . $db->real_escape_string($color) . "', '" . $db->real_escape_string($ip) . "', '" . $db->real_escape_string($salt) . "', '" . $db->real_escape_string($verified) . "')");
-						
-  if (!$result) {
-    echo $lang["error.Database"];
-  }
-  elseif ($config["registration"] == "open") {
-    $result = $db->query("SELECT userid, username, role FROM users WHERE username='" . $db->real_escape_string($username) . "'");
-
-    while ($row = $result->fetch_assoc()) {
-      $_SESSION['signed_in'] = true;
-      $_SESSION['userid'] = $row["userid"];
-      $_SESSION['username'] = $row["username"];
-      $_SESSION['role'] = $row["role"];
-    }
-
-    redirect("userpanel");
-  }
-  elseif ($config["registration"] == "approval") {
-    include 'header.php';
-    echo '<h2>' . $lang["register.Header"] . '</h2>';
-    printf($lang["register.Approval"], genURL("login"));
-    include "footer.php";
-    exit;
-  }
-}
-
 ?>
