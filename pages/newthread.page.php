f59a4354c2fc9403f15844db7e4620d7d1334b43
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2026-08-25 01:48:45 -0700

    Update newthread.page.php

--- pages/newthread.page.php
+++ pages/newthread.page.php
@@ -5,198 +5,126 @@
 // Only load the page if it's being loaded through the index.php file.
 if (!defined("INDEXED")) exit;
 
+if (get_role_from_session() == "Guest") {
+    include 'header.php';
+    message(sprintf($lang["newthread.LoginToCreate"], genURL("login")));
+    include "footer.php";
+    exit;
+}
+
+if (!(get_role_permissions() & PERM_CREATE_THREAD)) {
+     include 'header.php';
+     message($lang["newthread.SuspendCantCreate"]);
+     include "footer.php";
+     exit;
+}
+    
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $cat = $db->query("SELECT 1 FROM categories WHERE categoryid='" . $db->real_escape_string($_POST["category"]) . "'");
+		
+    if (mb_strlen(trim($_POST["title"])) < 1) {
+        $error = message($lang["newthread.TitleEmpty"],true); goto err;
+    }
+    if (mb_strlen(trim($_POST["title"])) > $config["maxCharsPerTitle"]) {
+        $error = message(sprintf($lang["newthread.TitleBig1"], $config["maxCharsPerTitle"]),true); goto err;
+    }
+    if (!mb_strlen($_POST["content"])) {
+        $error = message($lang["newthread.PostEmpty"],true); goto err;
+    }
+    if (mb_strlen($_POST["content"]) > $config["maxCharsPerPost"]) {
+        $error = message(sprintf($lang["newthread.PostBig1"], $config["maxCharsPerPost"]),true); goto err;
+    }
+    if ((!$cat) or ($cat->num_rows < 1)) {
+        $error = message($lang["newthread.InvalidCategory"],true); goto err;
+    }
+    $delaycheck = $db->query("SELECT 1 FROM posts WHERE user='" . $_SESSION["userid"] . "' AND timestamp>'" . (time() - $config["postDelay"]) . "'");
+    if ($delaycheck->num_rows > 0) {
+        $error = message(sprintf($lang["newthread.PostSoon1"], $config["postDelay"])); goto err;
+    }
+    $beginwork = $db->query("BEGIN WORK");
+    if (!$beginwork) {
+        $error = message($lang["newthread.CreateError"],true); goto err;
+    }
+    $justnow = time();
+    $userid = $_SESSION["userid"];
+    $draft = (int)isset($_POST["saveDraft"]);
+    
+    $threadresult = $db->query("INSERT INTO threads (title, sticky, locked, posts, startuser, starttime, lastpostuser, lastposttime, category, draft) VALUES ('" . $db->real_escape_string(trim($_POST["title"])) . "', '0', '0', '1', '$userid', '$justnow', '$userid', '$justnow', '" . $db->real_escape_string($_POST["category"]) . "', '" . $db->real_escape_string($draft) . "')");
+			
+    if (!$threadresult) {
+        $error = message($lang["newthread.InsertThreadError"],true);
+        $db->query("ROLLBACK"); goto err;
+    }
+    $threadid = $db->insert_id;
+    $result = $db->query("INSERT INTO posts (thread, user, timestamp, content) VALUES ('$threadid', '$userid', '$justnow', '" . $db->real_escape_string($_POST["content"]) . "')");
+    
+    if (!$result) {
+        $error = message($lang["newthread.InsertPostError"],true);
+        $db->query("ROLLBACK"); goto err;
+    }
+    $db->query("COMMIT");
+    redirect("thread/" . $threadid);
+    include 'header.php';
+    message($lang["newthread.SuccessCreate1"] . ' <a href="' . genURL('thread/' . $threadid) . '/">' . $lang["newthread.SuccessCreate2"] . '</a>');
+    include("footer.php");
+    exit;
+}
+
+err:
+
 include 'header.php';
 
-if($_SESSION['signed_in'] == false)
-{
-	message(sprintf($lang["newthread.LoginToCreate"], genURL("login")));
-	include "footer.php";
-	exit;
+$data = array(
+    "new_thread" => $lang["newthread.Header"],
+    "title_label" => $lang["newthread.Title"],
+    "title_maxlength" => $config["maxCharsPerTitle"],
+    "title_save" => "",
+    "category_label" => $lang["newthread.Category"],
+    "categories" => "",
+    "bbcode_tray" => BBCodeButtons(1,false),
+    "content_save" => "",
+    "create_label" => $lang["newthread.CreateBtn"],
+    "show_preview" => $lang["nav.ShowPreview"],
+    "hide_preview" => $lang["nav.HidePreview"],
+    "save_draft" => $lang["thread.PostSaveDraftBtn"]
+);
+
+if (isset($error)) {
+    $catSave = $_POST["category"];
+    $data["title_save"] = "value='" . trim(htmlspecialchars($_POST["title"])) . "'";
+    $data["content_save"] = htmlspecialchars($_POST["content"]);
+    echo $error;
 }
 
-if (!(get_role_permissions() & PERM_CREATE_THREAD))
-{
-	message($lang["newthread.SuspendCantCreate"]);
+$result = $db->query("SELECT * FROM categories");
+if (!$result) {
+    message($lang["newthread.DataError"]);
+    include 'footer.php';
+    exit;
 }
-else
-{
-    echo ("<script type='text/javascript' src='" . genURL("assets/thread.js") . "'></script>");
-    
-	if($_SERVER['REQUEST_METHOD'] == 'POST')
-	{
-		$cat = $db->query("SELECT 1 FROM categories WHERE categoryid='" . $db->real_escape_string($_POST["category"]) . "'");
-		
-		if (mb_strlen(trim($_POST["title"])) < 1)
-		{
-			message($lang["newthread.TitleEmpty"]);
-			$catSave = $_POST["category"];
-			$titleSave = trim($_POST["title"]);
-			$contentSave = $_POST["content"];
-		}
-		
-		elseif (mb_strlen(trim($_POST["title"])) > $config["maxCharsPerTitle"])
-		{
-            message(sprintf($lang["newthread.TitleBig1"], $config["maxCharsPerTitle"]));
-			$catSave = $_POST["category"];
-			$titleSave = trim($_POST["title"]);
-			$contentSave = $_POST["content"];
-		}
-		
-		elseif (mb_strlen($_POST["content"]) < 1)
-		{
-			message($lang["newthread.PostEmpty"]);
-			$catSave = $_POST["category"];
-			$titleSave = trim($_POST["title"]);
-			$contentSave = $_POST["content"];
-		}
-			
-		elseif (mb_strlen($_POST["content"]) > $config["maxCharsPerPost"])
-		{
-            message(sprintf($lang["newthread.PostBig1"], $config["maxCharsPerPost"]));
-			$catSave = $_POST["category"];
-			$titleSave = $_POST["title"];
-			$contentSave = $_POST["content"];
-		}
-		
-		elseif ((!$cat) or ($cat->num_rows < 1))
-		{
-			message($lang["newthread.InvalidCategory"]);
-			$catSave = $_POST["category"];
-			$titleSave = trim($_POST["title"]);
-			$contentSave = $_POST["content"];
-		}
-		
-		else
-		{
-			// First check and see if the user has made a post too recently according to the post delay.
-			$delaycheck = $db->query("SELECT 1 FROM posts WHERE user='" . $_SESSION["userid"] . "' AND timestamp>'" . (time() - $config["postDelay"]) . "'");
-			
-			if ($delaycheck->num_rows > 0)
-			{
-                message(sprintf($lang["newthread.PostSoon1"], $config["postDelay"]));
-				$catSave = $_POST["category"];
-				$titleSave = trim($_POST["title"]);
-				$contentSave = $_POST["content"];
-			}
-			
-			else
-			{
-		
-				$beginwork = $db->query("BEGIN WORK");
-		
-				if(!$beginwork)
-				{
-					message($lang["newthread.CreateError"]);
-				}
-		
-				else
-				{
-					$justnow = time();
-					$userid = $_SESSION["userid"];
-
-                    if (isset($_POST["saveDraft"])) {
-                        $draft = "1";
-                    }
-                    else {
-                        $draft = "0";
-                    }
-		
-					$threadresult = $db->query("INSERT INTO threads (title, sticky, locked, posts, startuser, starttime, lastpostuser, lastposttime, category, draft) VALUES ('" . $db->real_escape_string(trim($_POST["title"])) . "', '0', '0', '1', '$userid', '$justnow', '$userid', '$justnow', '" . $db->real_escape_string($_POST["category"]) . "', '" . $db->real_escape_string($draft) . "')");
-			
-					if (!$threadresult)
-					{
-						echo $lang["newthread.InsertThreadError"];
-						$db->query("ROLLBACK");
-					}
-			
-					else
-					{
-						$threadid = $db->insert_id;
-					 	
-						$result = $db->query("INSERT INTO posts (thread, user, timestamp, content) VALUES ('$threadid', '$userid', '$justnow', '" . $db->real_escape_string($_POST["content"]) . "')");
-				
-						if(!$result)
-						{
-							echo $lang["newthread.InsertPostError"];
-							$db->query("ROLLBACK");
-						}
-						else
-						{
-							$db->query("COMMIT");
-					
-							message($lang["newthread.SuccessCreate1"] . ' <a href="' . genURL('thread/' . $threadid) . '/">' . $lang["newthread.SuccessCreate2"] . '</a>');
-							include("footer.php");
-							redirect("thread/" . $threadid);
-							exit;
-						}
-					}
-				}
-			}
-		}
-	}
-	
-	echo '<h2>'.$lang["newthread.Header"].'</h2>';
-		
-	$result = $db->query("SELECT * FROM categories");
-		
-	if(!$result)
-	{
-		message($lang["newthread.DataError"]);
-	}
-		
-	else
-	{
-		if(!$result->num_rows)
-		{
-			if($_SESSION['role'] == "Administrator")
-			{
-				message($lang["newthread.NoCategoryAdmin"]);
-			}
-				
-			else
-			{
-				message($lang["newthread.NoCategoryUser"]);
-			}
-		}
-			
-		else
-		{
-		
-			echo '<form method="post" action="">';
-			echo '<div class="forminput"><label>' . $lang["newthread.Title"] . '</label><input type="text" maxlength="' . $config["maxCharsPerTitle"] . '" name="title"';
-			if (isset($titleSave)) echo "value='" . $titleSave . "'";
-			echo '></div><div class="forminput"><label>' . $lang["newthread.Category"] . '</label>'; 
-
-			echo '<select name="category">';
-				while($row = $result->fetch_assoc())
-				{
-					echo '<option ';
-					if (isset($catSave) && $catSave == $row["categoryid"]) echo "selected ";
-                    elseif (isset($q2) && $q2 == $row["categoryid"]) echo "selected ";
-					echo 'value="' . $row['categoryid'] . '">' . htmlspecialchars($row['categoryname']) . '</option>';
-				}
-			echo '</select></div>';	
-					
-			BBCodeButtons(1);
-			echo '<div class="forminput"><textarea name="content" id="textbox1">';
-			if (isset($contentSave)) echo htmlspecialchars($contentSave);
-			echo '</textarea></div>';
-            echo '<div id="previewbox1" class="previewbox" style="display:none"></div>';
-			echo '<div class="forminput"><div class="forminput left"><input type="submit" class="buttonbig" value="' . $lang["newthread.CreateBtn"] . '"> ';
-            echo '<input type="button" class="buttonbig previewbutton" id="showpreview1" value="' . $lang["nav.ShowPreview"] . '" onclick="previewPost(1, 1);">';
-            echo '<input type="button" class="buttonbig previewbutton" id="hidepreview1" value="' . $lang["nav.HidePreview"] . '" onclick="previewPost(1, 0);" style="display:none"></div>';
-            echo '<div class="draftbuttons"><input type="submit" class="buttonbig" name="saveDraft" value="'.$lang["thread.PostSaveDraftBtn"].'">';
-            echo '</div></form></div>';
-		}
-	}
+if (!$result->num_rows) {
+    if (get_role_permissions() & PERM_EDIT_CATEGORY) message($lang["newthread.NoCategoryAdmin"]);
+    else message($lang["newthread.NoCategoryUser"]);
+    include 'footer.php';
+    exit;
 }
+while ($row = $result->fetch_assoc()) {
+    $data["categories"] .= '<option ';
+    if (isset($catSave) && $catSave == $row["categoryid"]) $data["categories"] .= "selected ";
+    else if (isset($q2) && $q2 == $row["categoryid"]) $data["categories"] .= "selected ";
+    $data["categories"] .= 'value="' . $row['categoryid'] . '">' . htmlspecialchars($row['categoryname']) . '</option>';
+}
+
+echo ("<script type='text/javascript' src='" . genURL("assets/thread.js") . "'></script>");
+
+echo $template->render("templates/newthread.html",$data);
 
 include 'footer.php';
 
-// If the viewing user is logged in, update their last action.
-if ($_SESSION['signed_in'] == true)
+if (get_role_from_session() != "Guest")
 {
-	update_last_action("action.CreateAThread");
+    update_last_action("action.CreateAThread");
 }
 
 ?>
